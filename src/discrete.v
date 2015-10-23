a17671d56377fc98b9f7754241c2b7564c241cbd
Pierre-Yves Strub <pierre-yves@strub.nu>
2015-10-23 11:59:04 +0200

    Denumerable union of countable sets is countable.

--- src/discrete.v
+++ src/discrete.v
@@ -42,6 +42,14 @@ Definition asbool (P : Prop) :=
 Lemma asboolP (P : Prop): reflect P (asbool P).
 Proof. by rewrite /asbool; case: pselect=> h; constructor. Qed.
 
+Lemma asboolE (P : Prop) : asbool P -> P.
+Proof. by case: asboolP. Qed.
+
+(* -------------------------------------------------------------------- *)
+Lemma in_rsetE {T : Type} (P : T -> Prop) x :
+  x \mem {{ y | P y }} -> P x.
+Proof. by move/in_rset. Qed.
+
 (* -------------------------------------------------------------------- *)
 Section RSetSubtype.
 Section Def.
@@ -72,7 +80,8 @@ Canonical rset_sub_countType (T : countType) (E : {rset T}) :=
   Eval hnf in CountType (@rset_sub T E) (rset_sub_countMixin E).
 End RSetSubtype.
 
-Notation "[ 'rsub' E ]" := (@rset_sub _ E).
+Notation "[ 'rsub' E ]" := (@rset_sub _ E)
+  (format "[ 'rsub'  E ]").
 
 (* -------------------------------------------------------------------- *)
 Section Countable.
@@ -80,18 +89,73 @@ Variable (T : Type) (E : {rset T}).
 
 CoInductive countable : Type :=
   Countable
-    (rpickle   : [rsub E] -> nat)
+    (rpickle : [rsub E] -> nat)
     (runpickle : nat -> option [rsub E])
     of pcancel rpickle runpickle.
+
+Definition rpickle (c : countable) :=
+  let: Countable p _ _ := c in p.
+
+Definition runpickle (c : countable) :=
+  let: Countable _ p _ := c in p.
+
+Lemma rpickleK c: pcancel (rpickle c) (runpickle c).
+Proof. by case: c. Qed.
 End Countable.
 
 (* -------------------------------------------------------------------- *)
 Section CountableTheory.
-Variable (T : countType) (E : {rset T}).
-
-Lemma countable_countable : countable E.
+Lemma countable_countable (T : countType) (E : {rset T}) : countable E.
 Proof. by exists pickle unpickle; apply/pickleK. Qed.
+
+Section CanCountable.
+Variables (T : Type) (U : countType) (E : {rset T}).
+Variables (f : [rsub E] -> U) (g : U -> [rsub E]).
+
+Lemma can_countable : cancel f g -> countable E.
+Proof.
+pose p := pickle \o f; pose u n := omap g (unpickle n).
+move=> can_fg; apply (@Countable _ E p u) => x.
+by rewrite {}/u {}/p /= pickleK /= can_fg.
+Qed.
+End CanCountable.
+
+Section CountType.
+Variables (T : eqType) (E : {rset T}) (c : countable E).
+
+Definition countable_countMixin  := CountMixin (rpickleK c).
+Definition countable_choiceMixin := CountChoiceMixin countable_countMixin.
+
+Definition countable_choiceType :=
+  ChoiceType [rsub E] countable_choiceMixin.
+
+Definition countable_countType :=
+  CountType countable_choiceType countable_countMixin.
+End CountType.
 End CountableTheory.
+
+Notation "[ 'countable' 'of' c ]" := (countable_countType c)
+  (format "[ 'countable'  'of'  c ]").
+
+(* -------------------------------------------------------------------- *)
+Section CountableUnion.
+Variables (T : eqType) (E : nat -> {rset T}).
+
+Hypothesis cE : forall i, countable (E i).
+
+Lemma cunion_countable : countable {{ x | exists i, x \mem E i }}.
+Proof.
+pose S := { i : nat & [countable of cE i] }; set F := {{ x | _ }}.
+have H: forall (x : [rsub F]), exists i : nat, asbool (val x \mem E i).
+  by case=> /= x /asboolP /in_rset [i xEi]; exists i; apply/asboolP.
+have G: forall (x : S), asbool (val (tagged x) \mem F).
+  by case=> i [] /= x /asboolP xEi; apply/asboolP/in_rset; exists i.
+pose f (x : [rsub F]) : S :=
+  Tagged (fun i => [rsub E i]) (RSetSub (xchooseP (H x))).
+pose g (x : S) := RSetSub (G x).
+by have /can_countable: cancel f g by case=> x hx; apply/val_inj.
+Qed.
+End CountableUnion.
 
 (* -------------------------------------------------------------------- *)
 Section Summable.
