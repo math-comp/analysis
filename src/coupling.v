6c97153031de760b30f4c9bb8df553f9463a83c8
Pierre-Yves Strub <pierre-yves@strub.nu>
2016-04-24 12:14:46 +0200

    Better construction of \hat{F}^k.

--- src/coupling.v
+++ src/coupling.v
@@ -31,8 +31,18 @@ End TV.
 
 (* -------------------------------------------------------------------- *)
 Definition is_path_metric {T : Type} (d : T -> T -> nat) :=
-  [/\ forall x y, d x y = 0 -> x = y
-   & forall x z, 1 < d x z -> exists2 y, d x z = 1 & d x y = d x z + d x y]%N.
+  [/\ forall x y, d x y = 0 -> x = y, forall x, d x x = 0
+   & forall x z, 1 < d x z -> exists2 y, d x y = 1 & d x z = d x y + d y z]%N.
+
+(* -------------------------------------------------------------------- *)
+Section PathMetricTh.
+Context {T : eqType} (d : T -> T -> nat).
+
+Hypothesis ipm : is_path_metric d.
+
+Lemma ipm_eq0 s1 s2 : (d s1 s2 == 0%N) = (s1 == s2).
+Proof. by case: ipm=> h1 h2 _; apply/eqP/eqP=> [/h1|->]. Qed.
+End PathMetricTh.
 
 (* -------------------------------------------------------------------- *)
 Section Lift.
@@ -78,27 +88,32 @@ Hypothesis F_esp1 :
   forall s1 s2, d s1 s2 = 1%N ->
     \E_[F s1 s2] (fun xy => (d xy.1 xy.2)%:R) < b * (d s1 s2)%:R.
 
-Fixpoint Fx_r (n : nat) (s1 s2 : T) : {distr (T * T) / R} :=
-  if n is n.+1 then
-    match d s1 s2 with
-    | 0 => mkdistrd (fun r => (r.1 == r.2)%:R * f s1 r.1)
-    | 1 => F s1 s2
-    | _ =>
-       if @idP `[exists s, [&& d s1 s == 1 & d s1 s2 == d s1 s + d s s2]%N]
-          is ReflectT Px
-       then
-         let s   := xchooseb Px in
-         let mu1 := Fx_r n s1 s in
-         let mu2 := Fx_r n s s2 in
+Lemma pathE : forall s1 s2, `[exists p : seq T,
+  [&& path [rel r1 r2 | d r1 r2 == 1%N] s1 p & last s1 p == s2]].
+Proof.                          (* FIXME: break abstraction *)
+move=> s1 s2; move: {2}(d s1 s2) (erefl (d s1 s2)) => [/eqP|l].
+  by rewrite ipm_eq0 // => /eqP->; apply/existsbP; exists [::] => /=.
+elim: l s1 s2 => [|l ih] s1 s2 => [eq1_d|eq_d].
+  by apply/existsbP; exists [:: s2]; rewrite /= eq1_d !eqxx.
+case: dpm => _ _ /(_ s1 s2); rewrite eq_d => -[] // x eq1 eq2.
+have /eqP: d x s2 == l.+1 by rewrite -eqSS eq2 eq1 add1n.
+move/ih=> /existsbP[p] /andP[ih1 ih2]; apply/existsbP.
+by exists (x :: p) => /=; rewrite eq1 eqxx /= ih1 ih2.
+Qed.
 
-         mkdistrd (fun r => psum (fun v =>
-           (mu1 (r.1, v) * mu2 (v, r.2)) / f s v))
+Fixpoint Fx_r s (p : seq T) :=
+  match p with
+  | [::]    => mkdistrd (fun r => (r.1 == r.2)%:R * f s r.1)
+  | [:: s'] => F s s'
+  | s' :: p =>
+      let (mu1, mu2) := (F s s', Fx_r s' p) in
+      mkdistrd (fun r => psum (fun v =>
+        (mu1 (r.1, v) * mu2 (v, r.2)) / f s' v))
+  end.
 
-       else dnull
-    end
-  else dnull.
+Definition Fx s1 s2 :=
+  let p := xchooseb (pathE s1 s2) in Fx_r s1 p.
 
-Definition Fx    s1 s2 := Fx_r D s1 s2.
 Definition Fxk k r1 r2 := klift (fun r => Fx r.1 r.2) k (r1, r2).
 
 Goal forall s1 s2 k, tv (klift f k s1) (klift f k s2) < b^+k * D%:R.
