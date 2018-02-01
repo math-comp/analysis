ec0ce21730a2106ba4e981ccfa1bc796f7369f00
damien rouhling <damien.rouhling@inria.fr>
2018-02-01 13:43:14 +0100

    Add definition of uniform continuity

--- hierarchy.v
+++ hierarchy.v
@@ -138,6 +138,20 @@ Definition entourages {T : uniformType} : set (set (T * T)):=
   filter_from [set eps : R | eps > 0]
               (fun eps => [set xy | ball xy.1 eps xy.2]).
 
+Definition unif_cont (U V : uniformType) (f : U -> V) :=
+  (fun xy => (f xy.1, f xy.2)) @ entourages --> entourages.
+
+Lemma unif_contP (U V : uniformType) (f : U -> V) :
+  unif_cont f <-> forall e, e > 0 -> exists2 d, d > 0 &
+    forall x y, ball x d y -> ball (f x) e (f y).
+Proof.
+split=> fcont=> [e egt0|A [e egt0 e_A]]; last first.
+  by have /fcont [d ? fde] := egt0; exists d => // ? /fde ?; apply: e_A.
+have [|d dgt0 fde] := fcont [set xy | ball xy.1 e xy.2].
+  by rewrite near_simpl; exists e.
+by exists d => // x y ?; rewrite -[y]/((x, y).2) -{1}[x]/((x, y).1); apply: fde.
+Qed.
+
 Lemma locally_ballE {M : uniformType} : locally_ (@ball M) = locally.
 Proof. by case: M=> [?[?[]]]. Qed.
 
