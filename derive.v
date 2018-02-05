ff1a3cb585654b81b47a772af9f59d47fa110b63
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2018-02-05 16:56:30 +0900

    generalized littleo_id0 to littleo_lim0

--- derive.v
+++ derive.v
@@ -107,14 +107,32 @@ Section diff_locally_converse_tentative.
 (* if there exist A and B s.t. f(a + h) = A + B h + o(h) then
    f is differentiable at a, A = f(a) and B = f'(a) *)
 
-(* Prove more generally that if f @ x --> 0 then 'O_x f x = 0. *)
-(* statement: Lemma littleo_id (f : R^o -> R^o) (h : _ -> R^o) (x : R^o) :
-  f @ x --> (0 : R^o) -> [O_(x : R^o) f of h] x = 0.*)
-Lemma littleo_id0 (h : _ -> R^o) : [o_ (0 : R^o) id of h] 0 = 0.
+Lemma normm_littleo x (f : _ -> R^o) :
+  `|[ [o_x (cst 1 : R^o -> R^o) of f] x ]| = 0.
+Proof.
+rewrite /the_littleo val_insubd; case: ifPn => [/asboolP H|*]; last first.
+  by rewrite normm0.
+case : (ltrP 0 `|[f x]|) => [fx0|]; last first.
+  by rewrite normm_le0 => /eqP ->; rewrite normm0.
+have {H}/H/locally_singleton : 0 < `|[f x]| / (`|[ 1 : R^o ]| * 2%:R).
+  by rewrite divr_gt0 // mulr_gt0 // normm_gt0.
+rewrite invrM ?unitfE ?normm_eq0 // -2!mulrA mulVr ?unitfE ?normm_eq0 // mulr1.
+by rewrite ler_pmulr // invr_ge1 // ?unitfE // (@ler_nat _ _ 1).
+Qed.
+
+Lemma littleo_lim0 (f : R^o -> R^o) (h : _ -> R^o) (x : R^o) :
+  f @ x --> (0 : R^o) -> [o_ (x : R^o) f of h] x = 0.
+Proof.
+move/eqolim0P => ->.
+set k := 'o _; have /(_ _ [gt0 of 1])/= := littleoP [littleo of k].
+by move=> /locally_singleton; rewrite mul1r normm_littleo normm_le0 => /eqP.
+Qed.
+
+(*Lemma littleo_id0 (h : _ -> R^o) : [o_ (0 : R^o) id of h] 0 = 0.
 Proof.
 set k := 'o _; have /(_ _ [gt0 of 1])/= := littleoP [littleo of k].
 by move=> /locally_singleton; rewrite mul1r normm0 normm_le0 => /eqP.
-Qed.
+Qed.*)
 
 (* this is a consequence of diff_continuous and eqolim0 *)
 (* indeed the differential beeing b *: idfun is locally bounded *)
@@ -124,7 +142,7 @@ Lemma diff_locally_converse_part1 (f : R^o -> R^o) (a b : R^o) (x : R^o) :
   f \o shift x = cst a + b *: idfun +o_ (0 : R^o) id -> f x = a.
 Proof.
 rewrite funeqE => /(_ 0) /=; rewrite add0r => ->.
-by rewrite -[LHS]/(_ 0 + _ 0 + _ 0) /cst [X in a + X]scaler0 littleo_id0 !addr0.
+by rewrite -[LHS]/(_ 0 + _ 0 + _ 0) /cst [X in a + X]scaler0 littleo_lim0 ?addr0.
 Qed.
 
 End diff_locally_converse_tentative.
