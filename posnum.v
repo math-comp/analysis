8bd573c10c9221db91b04e47b21309bbe8e8014a
damien rouhling <damien.rouhling@inria.fr>
2018-02-07 16:51:12 +0100

    Improve the documentation, remove duplicate notation, fix acknowledgements

--- posnum.v
+++ posnum.v
@@ -8,8 +8,17 @@ From SsrReals Require Import boolp reals.
 (* This file develops tools to make the manipulation of positive numbers      *)
 (* easier, thanks to canonical structures.                                    *)
 (*                                                                            *)
+(*          !! x == triggers pretyping to fill the holes of the term x. The   *)
+(*                  main use case is to trigger typeclass inference in the    *)
+(*                  body of a ssreflect have := !! body.                      *)
+(*                  Credits: Enrico Tassi.                                    *)
 (*    {posnum R} == interface type for elements in R that are positive; R     *)
 (*                  must have a numDomainType structure.                      *)
+(*                  Allows to solve automatically goals of the form x > 0 if  *)
+(*                  x is canonically a {posnum R}. {posnum R} is canonically  *)
+(*                  stable by addition, multiplication, inverse, min and sqrt *)
+(*                  All positive natural numbers ((n.+1)%:R) are also         *)
+(*                  canonically in {posnum R}                                 *)
 (*   PosNum xgt0 == packs the proof xgt0 : x > 0, for x : R, to build a       *)
 (*                  {posnum R}.                                               *)
 (*        x%:pos == explicitely casts x to {posnum R}, triggers the inference *)
@@ -97,8 +106,6 @@ Canonical oner_posnum := PosNum one_pos_gt0.
 End PosNum.
 Hint Extern 0 ((0 <= _)%R = true) => exact: posnum_ge0 : core.
 Hint Extern 0 ((_ != 0)%R = true) => exact: posnum_neq0 : core.
-Notation "[gt0 'of' x ]" := (@posnum_gt0_def _ _ (Phantom R x))
- (format "[gt0 'of'  x ]").
 
 Section PosNumReal.
 Context {R : realDomainType}.
