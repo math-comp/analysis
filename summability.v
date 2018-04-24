29922439e6abc4da03d53d54cd1f86bcce9104a1
Cyril Cohen <cohen@crans.org>
2018-04-24 09:46:05 +0200

    moving summability to a new file

---
+++ summability.v
@@ -0,0 +1,50 @@
+(* mathcomp analysis (c) 2017 Inria and AIST. License: CeCILL-C.              *)
+Require Import Reals.
+From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat eqtype choice.
+From mathcomp Require Import seq fintype bigop ssralg ssrint ssrnum finmap.
+From mathcomp Require Import matrix interval zmodp.
+Require Import boolp reals.
+Require Import Rstruct Rbar set posnum topology hierarchy.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+Import GRing.Theory Num.Def Num.Theory.
+
+Local Open Scope classical_set_scope.
+
+(** For Pierre-Yves : definition of sums *)
+
+From mathcomp Require fintype bigop finmap.
+
+Section totally.
+
+Import fintype bigop finmap.
+Local Open Scope fset_scope.
+(* :TODO: when eventually is generalized to any lattice *)
+(* totally can just be replaced by eventually *)
+Definition totally {I : choiceType} : set (set {fset I}) :=
+  filter_from setT (fun A => [set B | A `<=` B]).
+
+Canonical totally_filter_source {I : choiceType} X :=
+  @Filtered.Source X _ {fset I} (fun f => f @ totally).
+
+Instance totally_filter {I : choiceType} : ProperFilter (@totally I).
+Proof.
+eapply filter_from_proper; last by move=> A _; exists A; rewrite fsubset_refl.
+apply: filter_fromT_filter; first by exists fset0.
+by move=> A B /=; exists (A `|` B) => P /=; rewrite fsubUset => /andP[].
+Qed.
+
+Definition partial_sum {I : choiceType} {R : zmodType}
+  (x : I -> R) (A : {fset I}) : R := \sum_(i : A) x (val i).
+
+Definition sum (I : choiceType) {K : absRingType} {R : normedModType K}
+   (x : I -> R) : R := lim (partial_sum x).
+
+Definition summable (I : choiceType) {K : absRingType} {R : normedModType K}
+   (x : I -> R) :=
+   \forall M \near +oo, \forall J \near totally,
+   partial_sum (fun i => `|[x i]|) J <= M.
+
+End totally.
