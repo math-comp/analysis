56d5a0937116358a1a6c01a0ad297470dead157b
Cyril Cohen <cohen@crans.org>
2018-04-24 09:46:05 +0200

    moving summability to a new file

--- hierarchy.v
+++ hierarchy.v
@@ -2843,34 +2843,3 @@ Qed.
 Lemma locally_pt_comp (P : R -> Prop) (f : R -> R) (x : R) :
   locally (f x) P -> continuity_pt f x -> \near x, P (f x).
 Proof. by move=> Lf /continuity_pt_flim; apply. Qed.
-
-(** For Pierre-Yves : definition of sums *)
-
-From mathcomp Require fintype bigop finmap.
-
-Section totally.
-
-Import fintype bigop finmap.
-Local Open Scope fset_scope.
-(* :TODO: when eventually is generalized to any lattice  by any lattice *)
-(* totally can just be replaced by eventually *)
-Definition totally {I : choiceType} : set (set {fset I}) :=
-  filter_from setT (fun A => [set B | A `<=` B]).
-
-Canonical totally_filter_source {I : choiceType} X :=
-  @Filtered.Source X _ {fset I} (fun f => f @ totally).
-
-Instance totally_filter {I : choiceType} : ProperFilter (@totally I).
-Proof.
-eapply filter_from_proper; last by move=> A _; exists A; rewrite fsubset_refl.
-apply: filter_fromT_filter; first by exists fset0.
-by move=> A B /=; exists (A `|` B) => P /=; rewrite fsubUset => /andP[].
-Qed.
-
-Definition partial_sum {I : choiceType} {R : zmodType}
-  (x : I -> R) (A : {fset I}) : R := \sum_(i : A) x (val i).
-
-Definition sum (I : choiceType) {K : absRingType} {R : normedModType K}
-   (x : I -> R) : R := lim (partial_sum x).
-
-End totally.
