49f7b3a857d00a9cca507d081c6c652d9a8be4bc
Marie Kerjean <marie.kerjean@gmail.com>
2018-12-21 11:54:22 +0100

    init

---
+++ hahn_banach.v
@@ -0,0 +1,12 @@
+From mathcomp Require Import all_ssreflect all_algebra.
+(* ------- *) Require Import boolp classical_sets reals.
+
+
+Local Open Scope ring_scope.
+Import GRing.Theory Num.Theory.
+
+Section test.
+Context {R : realType}.
+
+
+End test.
