3d012c252b2270b9be4a6cee8bdbbaa32556349b
mkerjean <kerjean@irif.fr>
2019-05-25 22:26:39 +0200

    more

--- hahn_banach_applications.v
+++ hahn_banach_applications.v
@@ -18,9 +18,11 @@ Import GRing.Theory Num.Def Num.Theory .
 Local Open Scope ring_scope .
 Local Open Scope classical_set_scope.
 
+Check in_set.
+
 Section LinearContinuousBounded.
 
-Variables (V W : normedModType R ) . 
+Variables (V W : normedModType R ) ( U : uniformType) . 
 
 Definition continuous_at x (f : V -> W) :=   ( f @ x) --> (f x).
 
@@ -32,33 +34,26 @@ Proof.
    - by move => /flim_ballPpos. 
 Qed.
 
-
-
-(*Lemma locallyP (Z : normedModType R ) (x : Z) A : (locally x A) <-> (exists r:posreal, ball x r%:num `<=` A).
-Proof.
-Admitted.  *)
-
 Definition continuous_on F f :=  forall x , F x -> (continuous_at x f ).
 
-Lemma div_abs ( x y : R) : `| x^-1 * y |%real = `| x^1 | * `| y| . 
-Proof.
-  Search _ ( `| _ | * `| _ |). rewrite -normedtype.R_AbsRingMixin_obligation_1.
- Search   (_%coqR) in reals.  
-Admitted.
 
+(* Shouldn't that be replaced by (continuous at 0 f )<-> =0 O (cst) *) 
 Lemma continuous_bounded0 (f : { linear V -> W}) :
   (continuous_at 0 f) -> 
   (*( forall z, (filtermap f (locally z) `=>` ( locally (f z)))  )*)
   ( exists  r , (r > 0 ) /\ (forall x : V,   ( `|[f x]| ) <=  (`|[x]| ) * r  )  ) . 
 Proof.
   move => /continuous_atP H. have H' : (0 < 1) by []. move : (H (1%:pos))  {H'} .
-  rewrite (linear0 f) /( _ @ _ ) //=. move => H'{H}. Check (locally_ ball 0). 
+  rewrite (linear0 f) /( _ @ _ ) //=. move => H'{H}. 
   (* H'= locally_ ball 0 (fun a => ball 0 1 (f a )).*)
   have : exists t , (t > 0) /\  forall x , ball 0 t x -> ball 0 1 (f x ).
-    rewrite -!ball_normE /ball_. (*rewrite sub0r*) admit. 
+    rewrite -!ball_normE /ball_.  (*rewrite sub0r*) admit.  
 
   move => [t [tp H]].
-  exists t^-1. split. by rewrite invr_gt0.
+  exists (2*t^-1). split.  rewrite mulr_gt0.
+    by [].
+    by [].
+    by rewrite invr_gt0.
   move => x ; case :  (boolp.EM (x=0)).
   - by move => -> ; rewrite (linear0 f) !normm0 //= mul0r. 
   - move => xneq0. 
@@ -67,24 +62,41 @@ Proof.
     case : (boolp.EM (x==0)).
     - move => /eqP x0. by have : False by apply : ( xneq0 x0). 
     - by move => /negP. 
-    pose a := (  `|[x]|^-1 * t ) *: x.
+    pose a := (  `|[x]|^-1 * t/2 ) *: x.
     (*going from ball to norm is done through ball_normE and unfolding ball_. 
     I looked a long time for this *)
     have ball0ta : ball 0 t a. 
-     apply : ball_sym ; rewrite -ball_normE /ball_ subr0.
-     rewrite normmZ . admit.
-     (* stuck with %real , search a view*)(* rewrite normedtype.R_AbsRingMixin_obligation_1. *)
-    move : (H a ball0ta) ;rewrite -ball_normE /ball_ sub0r normmN. 
-    have  : ( f a =  ( (`|[x]|^-1) * t ) *: ( f x)) .
-      have : a = (`|[x]|^-1 * t) *: x + 0 by rewrite addrC add0r.  
-      by move => -> ; rewrite (linearP f  (`|[x]|^-1 * t) x 0) (linear0 f) addrC add0r.  
+     apply : ball_sym ; rewrite -ball_normE /ball_  subr0.
+     rewrite normmZ absRE mulrC normedtype.R_AbsRingMixin_obligation_1.
+     rewrite !gtr0_norm. rewrite mulrA mulrA divrr. 
+     rewrite mul1r mulrC ltr_pdivr_mull. rewrite ltr_pmull.   
+      (* 1 <2 ??? *) admit.
+      by [].
+      by [].  
+      (* `|[x]| \is a GRing.unit *) admit.
+      by  rewrite invr_gt0.
+      apply : mulr_gt0. 
+       by rewrite invr_gt0.
+       by [].
+    move : (H a ball0ta) ;rewrite -ball_normE /ball_ sub0r normmN {ball0ta}. 
+    have  : ( f a =  ( (`|[x]|^-1) * t/2 ) *: ( f x)) .
+      have : a = (`|[x]|^-1 * t/2) *: x + 0 by rewrite addrC add0r.  
+      by move => -> ; rewrite linearP (linear0 f) addrC add0r.   
     move => -> . 
-    rewrite normmZ (div_abs `|[x]| t) (gtr0_norm tp) (ger0_norm (normm_ge0 x)).
-    rewrite mulrC mulrC -mulrA.  
-    rewrite (ltr_pdivr_mull 1  (t*`|[f x]|) normxle0 ).
-    rewrite mulrC (mulrC `|[x]| 1) mul1r -(ltr_pdivl_mulr `|[f x]| `|[x]| tp).
-    by move => Ht ; apply : ltrW. 
+    rewrite normmZ absRE. rewrite normedtype.R_AbsRingMixin_obligation_1 !(gtr0_norm). 
+    rewrite mulrC mulrC  -mulrA. rewrite -mulrA  ltr_pdivr_mull. 
+    rewrite mulrC [(_*1)]mulrC mul1r -ltr_pdivl_mulr. rewrite ltr_pdivr_mull. 
+     move => Ht.  apply : ltrW ; rewrite mulrC -mulrA (mulrC t^-1). 
+      by [].
+      by [].
+      by []. 
+      by [].
+      by rewrite invr_gt0.  
+      apply : mulr_gt0. 
+       by rewrite invr_gt0.
+       by [].  
 Admitted.
+
 
 
 (*
@@ -109,7 +121,7 @@ Proof.
   (*use linear_continuous of landau and landau_cst_bounded*)
   move => H.
   apply :  linear_cont0_continuous . 
-  apply : (proj2 (continuous_atP 0 f)). move => A f0A. 
+  apply : (proj2 (continuous_atP 0 f)). 
 Admitted.
 
 End LinearContinuousBounded.
@@ -146,32 +158,38 @@ Proof.
   - move => x xmajA.
     have ubdA : is_upper_bound A x.  move => y Ay. Check (xmajA y Ay). (* Je suis dans coqR*) admit. 
     Check proj2 (real_sup_is_lub has_sup_A) x. admit. (* (cannot unify "is_true (in_set A x0)" and "A x0")*)
-Admitted.*)  
+Admitted.*)
 
+(*Unset Printing Notations.*)
+Set Printing Coercions.
+
+(* From the sup properties proven in bool in reals, we deduce the version in Prop 
+  that we used in our proof of hahn_banacH *)
 Lemma mymysup : forall (A : set R) (a m : R),
      A a -> ubd A m ->
      {s : R | ubd A s /\ forall u, ubd A u -> s <= u}.
 Proof.
-  move => A a  m  Aa majAm.  Search _ (Prop -> bool ).  
+  move => A a  m  Aa majAm.  
   have : (has_sup  (in_set A)).
   split . 
   - by exists a ; rewrite in_setE.
   - exists m ; rewrite in_setE // => x ;apply : propF ; rewrite negb_imply //= . 
     move  => /andP ; apply and_ind; rewrite in_setE  => Ax /negP nxm.
       by apply : (nxm (majAm x Ax)).
-  move => /has_supP has_sup_A.
-  pose s :=  (real_sup (in_set A)) ; exists s .
+  move => /has_supP has_sup_A {m majAm a Aa}. 
+  exists (real_sup (in_set A)).  
   split.  
-  - move => x Ax.  move : (real_sup_ub (has_sup_A)) => //= /ubP H.
+  - move => x Ax ; move : (real_sup_ub (has_sup_A)) => //= /ubP H.
     have Axb : x \in in_set A by rewrite in_setE.
     by apply : (H x Axb).  
   - move => x xmajA.
-    have ubdA : is_upper_bound A x.  move => y Ay. Check (xmajA y Ay).  admit.
-    (* cannot go in coqR*)
-    have majAmb : is_upper_bound  (fun x : R => in_set A x) m.
-    move => y ; rewrite in_setE => Ay. Check (xmajA y Ay).   admit. (* cannot go in coqR*)
-    Check proj2 (real_sup_is_lub has_sup_A) m majAmb.   admit. (* cannot go in coqR*)
-Admitted. 
+    have ubdA : is_upper_bound A x by move => y Ay ; move : (xmajA y Ay) ;  move => /RleP.   
+    (*I spent a long time looking for RleP*)
+    have majAxb : is_upper_bound  (fun x : R => in_set A x) x.
+    by move =>y ; rewrite in_setE => Ay ; move : (xmajA y Ay) ; move => /RleP.   
+    move : (proj2 (real_sup_is_lub has_sup_A) x majAxb).
+    by move => /RleP.
+Qed.     
  
 
 Lemma mymyinf : forall (A : set R) (a m : R),
@@ -179,16 +197,18 @@ Lemma mymyinf : forall (A : set R) (a m : R),
      {s : R | ibd A s /\ forall u, ibd A u -> u <= s}.
 Admitted.
 
+Check (continuous f).
+
 
 Notation myHB := (hahn_banach.HahnBanach (boolp.EM) Choice_prop mymysup mymyinf) .
 
 (*  need to give  scalar R is of type V -> W *)
 
-(*Theorem HB_geom_normed :
+Theorem HB_geom_normed :
   (forall x , F x -> continuous_at x f)
-   -> exists g : {scalar V} , ( continuous f ) /\ ( forall x, F x -> (f x = g x) ) .  
+   -> exists g : {scalar V} , ( continuous g ) /\ ( forall x, F x -> (f x = g x) ) .  
  Proof.  
- Admitted.  *)
+ Admitted. 
 
 End HBGeomNormed.
 
