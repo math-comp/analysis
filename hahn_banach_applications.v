ec8160db19ccb88de2adf87f9d05bab37471e705
mkerjean <kerjean@irif.fr>
2019-05-27 21:26:38 +0200

    continuous -> bounded - missing 2 is a Gring.unit

--- hahn_banach_applications.v
+++ hahn_banach_applications.v
@@ -17,14 +17,15 @@ Import GRing.Theory Num.Def Num.Theory .
 
 Local Open Scope ring_scope .
 Local Open Scope classical_set_scope.
-
-Check in_set.
+ 
 
 Section LinearContinuousBounded.
 
-Variables (V W : normedModType R ) ( U : uniformType) . 
+Variables (V W : normedModType R ) . 
 
 Definition continuous_at x (f : V -> W) :=   ( f @ x) --> (f x).
+
+Definition continuousR_at x (f : V -> R) :=   ( f @ x) --> (f x).
 
 Lemma continuous_atP x (f : V -> W) :
   (continuous_at x f) <-> forall eps : posreal, \forall y \near f @ x, ball (f x) eps%:num y.
@@ -34,18 +35,26 @@ Proof.
    - by move => /flim_ballPpos. 
 Qed.
 
+Lemma continuousR_atP x (f : V -> R) :
+  (continuousR_at x f) <-> forall eps : posreal, \forall y \near f @ x, ball (f x) eps%:num y.
+Proof.
+  rewrite /continuous_at. split.
+   - by move => /flim_ballPpos. 
+   - by move => /flim_ballPpos. 
+Qed.
+
 Definition continuous_on F f :=  forall x , F x -> (continuous_at x f ).
 
+Definition continuousR_on F f :=  forall x , F x -> (continuous_at x f ).
 
-(* Shouldn't that be replaced by (continuous at 0 f )<-> =0 O (cst) *) 
-Lemma continuous_bounded0 (f : { linear V -> W}) :
+
+(* Case for linear function into normed spaces, slightly different than real functions*)
+(*Lemma continuous_bounded0 (f : { linear V -> W}) :
   (continuous_at 0 f) -> 
-  (*( forall z, (filtermap f (locally z) `=>` ( locally (f z)))  )*)
-  ( exists  r , (r > 0 ) /\ (forall x : V,   ( `|[f x]| ) <=  (`|[x]| ) * r  )  ) . 
+   ( exists  r , (r > 0 ) /\ (forall x : V,   ( `|[f x]| ) <=  (`|[x]| ) * r  )  ) . 
 Proof.
   move => /continuous_atP H. have H' : (0 < 1) by []. move : (H (1%:pos))  {H'} .
   rewrite (linear0 f) /( _ @ _ ) //=. move => H'{H}. 
-  (* H'= locally_ ball 0 (fun a => ball 0 1 (f a )).*)
   have : exists t , (t > 0) /\  forall x , ball 0 t x -> ball 0 1 (f x ).
     rewrite -!ball_normE /ball_.  (*rewrite sub0r*) admit.  
 
@@ -95,24 +104,72 @@ Proof.
       apply : mulr_gt0. 
        by rewrite invr_gt0.
        by [].  
-Admitted.
+Admitted.*)
 
 
 
-(*
-Lemma landau_cst_bounded ( g : V -> W) :
-  ( (g : _ -> _) =O_ (0 : V) (cst (1 : R^o))=O_0 cst 1) ->
-  exists  r : posreal, (forall x : V,   ( `|[f x]| ) <=    (`|[x]| ) * r%:num  ) .
-
-*)
-
-  
-Lemma linear_cont0_continuous  (f : { linear V -> W}) :
-  continuous_at 0 f -> continuous f.
+Lemma continuousR_bounded0 (f : {scalar V}) :
+  (continuousR_at 0 f) -> 
+   ( exists  r , (r > 0 ) /\ (forall x : V,   ( `|f x| ) <=  (`|[x]| ) * r  )  ) . 
 Proof.
+  move => /continuousR_atP H.
+  have  H':  (0 < 1) by []. move : (H (1%:pos)) {H'}.
+  rewrite (linear0 f) /( _ @ _ ) //= nearE => H0 {H}.
+  (* I had a hard time finding nearE as it is not in the abstract *)
+  move : (locally_ex H0) => [ tp  H] {H0}.
+  move : H. 
+  pose t := tp%:num. 
+  exists (2*t^-1). split.  rewrite mulr_gt0.
+    by [].
+    by [].
+    by rewrite invr_gt0.
+  move => x ; case :  (boolp.EM (x=0)).
+  -  by move => -> ; rewrite (linear0 f) normm0 normr0 //= mul0r. 
+  - move => xneq0. 
+    have normxle0 : `|[x]| > 0 .
+      rewrite normm_gt0 ; case : (boolp.EM (x==0)).
+    (*proving (x <> y) -> (x != y), where is it done ? *)
+    - by move => /eqP x0 ; have : False by apply : ( xneq0 x0). 
+    - by move => /negP. 
+  pose a := (  `|[x]|^-1 * t/2 ) *: x.
+  (*going from ball to norm is done through ball_normE and unfolding ball_. *)
+  (* I looked a long time for this *) 
+  have ball0ta : ball 0 t a.
+   apply : ball_sym ; rewrite -ball_normE /ball_  subr0.
+   rewrite normmZ absRE mulrC normedtype.R_AbsRingMixin_obligation_1.
+   rewrite !gtr0_norm. rewrite mulrC.   rewrite -mulrA -mulrA. rewrite ltr_pdivr_mull. rewrite mulrC. rewrite -mulrA. Search _ ( _ * _ < _ ). rewrite gtr_pmull. rewrite invr_lt1.
+     by have lt01 : 0 < 1 by [] ; have le11 : 1 <= 1 by [] ; apply : ltr_spaddr.
+     (* 2 Gring.Unit ?*) Search _ (_ \is a unit). admit.
+     by [].
+     by  rewrite mulr_gt0.
+     by []. 
+     by rewrite invr_gt0.
+      apply : mulr_gt0. by rewrite invr_gt0.
+     by [].
+  move : (H a ball0ta) ;  rewrite ball_absE /ball_ sub0r absRE normrN. 
+  have  : ( f a =  ( (`|[x]|^-1) * t/2 ) * ( f x)) .
+  have : a = (`|[x]|^-1 * t/2) *: x + 0 by rewrite addrC add0r.  
+   by move => -> ; rewrite linearP (linear0 f) addrC add0r.   
+  move => -> .
+  rewrite normedtype.R_AbsRingMixin_obligation_1 (gtr0_norm).  
+  rewrite mulrC mulrC  -mulrA. rewrite -mulrA  ltr_pdivr_mull.  
+  rewrite mulrC [(_*1)]mulrC mul1r -ltr_pdivl_mulr.
+  rewrite invf_div => Ht.
+   by apply : ltrW. 
+     by apply : mulr_gt0.
+     by [].
+     apply : mulr_gt0. 
+     apply : mulr_gt0.
+     by rewrite invr_gt0.
+     by [].  
+     by []. 
 Admitted.
-  
 
+Lemma landauR_cst_bounded ( g : {scalar V } ):
+  ( g  =O_0 cst 1) <->
+  exists  r , (r > 0) /\ (forall x : V,   ( `|g x| ) <=    (`|[x]| ) * r ) .
+
+  
 
 Lemma bounded_continuous  (f : { linear V -> W}) : 
   ( exists  r : posreal, (forall x : V,   ( `|[f x]| ) <=   (`|[x]| ) * r%:num  )  ) ->
@@ -122,6 +179,18 @@ Proof.
   move => H.
   apply :  linear_cont0_continuous . 
   apply : (proj2 (continuous_atP 0 f)). 
+Admitted.
+
+
+
+Lemma bounded_continuousR  (f : {scalar V}) : 
+  ( exists  r : posreal, (forall x : V,   ( `|f x| ) <=   (`|[x]| ) * r%:num  )  ) ->
+  continuous f.  
+Proof.
+  (*use linear_continuous of landau and landau_cst_bounded*)
+  move => H.
+  apply :  linear_cont0_continuousR . 
+  apply : (proj2 (continuousR_atP 0 f)). 
 Admitted.
 
 End LinearContinuousBounded.
@@ -140,28 +209,9 @@ Definition ubd (A : set R) (a : R) := forall x, A x -> x <= a.                  
  (* Lower bound *)
 Definition ibd (A : set R) (a : R) := forall x, A x -> a <= x. (*ib in reals*) 
 
-(*Lemma mysup : forall (A : set R) (a m : R),
-     A a -> ub (in_set A) m ->
-     {s : R | ub (in_set A) s /\ forall u, ub (in_set A) u -> s <= u}.
-Proof.
-  move => A a  m  Aa majAm.  
-  have : (has_sup  (in_set A)).  (*It seems that I havent imported somtehing from classical sets here, allowing for a pred structure on sets *) 
-  split . 
-  - by exists a ; rewrite in_setE.
-  - exists m ; rewrite in_setE // => x ;apply : propF ; rewrite negb_imply //= . 
-    move  => /andP ; apply and_ind; rewrite in_setE  => Ax /negP nxm.
-    Check (ub (in_set A)).   by apply : (nxm (majAm x Ax)).
-  move => /has_supP has_sup_A. pose s :=  (real_sup (in_set A)).
-  exists s  => //=. 
-  split.  
-  - move => x Ax.  move : (real_sup_ub (has_sup_A)) => //=.   admit.
-  - move => x xmajA.
-    have ubdA : is_upper_bound A x.  move => y Ay. Check (xmajA y Ay). (* Je suis dans coqR*) admit. 
-    Check proj2 (real_sup_is_lub has_sup_A) x. admit. (* (cannot unify "is_true (in_set A x0)" and "A x0")*)
-Admitted.*)
 
 (*Unset Printing Notations.*)
-Set Printing Coercions.
+(*Set Printing Coercions.*)
 
 (* From the sup properties proven in bool in reals, we deduce the version in Prop 
   that we used in our proof of hahn_banacH *)
@@ -197,17 +247,15 @@ Lemma mymyinf : forall (A : set R) (a m : R),
      {s : R | ibd A s /\ forall u, ibd A u -> u <= s}.
 Admitted.
 
-Check (continuous f).
-
 
 Notation myHB := (hahn_banach.HahnBanach (boolp.EM) Choice_prop mymysup mymyinf) .
 
-(*  need to give  scalar R is of type V -> W *)
-
 Theorem HB_geom_normed :
-  (forall x , F x -> continuous_at x f)
+  (forall x , F x -> continuousR_at x f)
    -> exists g : {scalar V} , ( continuous g ) /\ ( forall x, F x -> (f x = g x) ) .  
- Proof.  
+Proof.  
+
+
  Admitted. 
 
 End HBGeomNormed.
