c8ae19bc6bb906026d246898cca824749e8f97e7
Cyril Cohen <cohen@crans.org>
2019-10-02 14:05:34 +0200

    travis on nix

---
+++ default.nix
@@ -0,0 +1,57 @@
+{withEmacs ? false,
+ nixpkgs ? (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/c4196cca9acd1c51f62baf10fcbe34373e330bb3.tar.gz";
+  sha256 = "0jsisiw8yckq96r5rgdmkrl3a7y9vg9ivpw12h11m8w6rxsfn5m5";
+}),
+coq-version ? "8.9",
+mc ? {url = "https://github.com/math-comp/math-comp"; ref = "experiment/order";},
+print-env ? false
+}:
+let
+  config.packageOverrides = pkgs:
+    with pkgs; {
+      coqPackages =
+        let coqPackages = (with pkgs;{
+              "8.7" = coqPackages_8_7;
+              "8.8" = coqPackages_8_8;
+              "8.9" = coqPackages_8_9;
+              "8.10" = coqPackages_8_10;
+            }."${coq-version}");
+        in
+        coqPackages.overrideScope' (self: super: {
+          mathcomp = super.mathcomp.overrideMathcomp (o: {
+            version = "dev";
+            name = "coq${super.coq.coq-version}-mathcomp-dev";
+            src = fetchGit mc;
+          });
+        });
+      emacs = emacsWithPackages (epkgs:
+        with epkgs.melpaStablePackages; [proof-general]);
+    };
+in
+with import nixpkgs {inherit config;};
+stdenv.mkDerivation rec {
+  name = "mathcomp-analysis";
+
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [ coq ] ++ (with coqPackages;
+    [mathcomp mathcomp-finmap mathcomp-bigenough
+     mathcomp-multinomials mathcomp-real-closed coqeal])
+                ++ lib.optional withEmacs emacs;
+
+  src = ./. ;
+
+  meta = {
+    inherit (coqPackages.mathcomp.meta) platforms;
+    maintainers = [ stdenv.lib.maintainers.ccohen ];
+  };
+
+  shellHook = ''
+    nixEnv (){
+      echo "Here is your work environement:"
+      for x in $buildInputs; do printf "  "; echo $x | cut -d "-" -f "2-"; done
+      echo "you can pass option '--argstr coq-version \"x.y\"' to nix-shell to change coq versions"
+    }
+  '' + lib.optionalString print-env "nixEnv";
+}
