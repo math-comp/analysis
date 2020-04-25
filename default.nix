e1fa1d192352046ff676ea600d97e2619642f6ce
Cyril Cohen <cohen@crans.org>
2020-04-25 23:04:47 +0200

    refresh nix shell and build

--- default.nix
+++ default.nix
@@ -1,25 +1,28 @@
 {
-  nixpkgs ? (fetchTarball https://github.com/CohenCyril/nixpkgs/archive/d53b12e0fc3b8941dfcf28e097600aec3b7b5c0e.tar.gz),
-  coq-version ? "8.10",
-  mc-version ? "1.11.0+beta1",
+  nixpkgs ? (fetchTarball https://github.com/CohenCyril/nixpkgs/archive/mathcomp-1.11.tar.gz),
+  config ? (pkgs: {}),
   withEmacs ? false,
   print-env ? false,
-  config ? pkgs: coqPackages: {},
-  name ? (config {} {}).name or "analysis",
-  src ? (config {} {}).src or ./.,
+  package ? "analysis",
+  src ? (config.${package}.version or ./.),
 }:
+with builtins;
 let
+  cfg-fun = if isFunction config then config else (_: config);
   pkgs = import nixpkgs {
-    config.packageOverrides = pkgs: with pkgs; {
-      coqPackages = {
+    config.packageOverrides = pkgs: with pkgs.lib; {
+      coqPackages = with pkgs; {
         "8.7" = coqPackages_8_7;
         "8.8" = coqPackages_8_8;
         "8.9" = coqPackages_8_9;
         "8.10" = coqPackages_8_10;
         "8.11" = coqPackages_8_11;
-      }.${coq-version}.overrideScope'
+        "default" = coqPackages_8_11;
+      }.${(cfg-fun pkgs).coq or "default"}.overrideScope'
         (self: super:
-          let mathcomp-full-config = lib.recursiveUpdate super.mathcomp-extra-config {
+          let
+            cfg = cfg-fun (pkgs // {coqPackages = self;});
+            mathcomp-full-config = lib.recursiveUpdate super.mathcomp-extra-config {
                 for-package = {
                   mathcomp-fast = version: {
                     propagatedBuildInputs = with self; ([ mathcomp ] ++ mathcomp-extra-fast);
@@ -31,30 +34,29 @@ let
               };
           in {
             mathcomp-extra-config = lib.recursiveUpdate mathcomp-full-config {
-              for-package = {
-                ${name} = version:
-                  lib.recursiveUpdate ((mathcomp-full-config.for-package.${name}
-                    or (version: {})) version) (config pkgs self);
-              };
+              for-package = mapAttrs
+                (name: value: version: recursiveUpdate (value version) cfg)
+                (filterAttrs (name: _: !elem name [ "mathcomp" ]) cfg);
             };
-            mathcomp = self.mathcomp_ mc-version;
+            mathcomp = if cfg?mathcomp then self.mathcomp_ cfg.mathcomp else self.mathcomp_ "1.11.0+beta1";
           });
     };
   };
-
-  current-config = config pkgs pkgs.coqPackages;
 
   mathcompnix = ./.;
 
   shellHook = ''
     nixEnv () {
-      echo "Here is your work environement:"
+      echo "Here is your work environement"
+      echo "buildInputs:"
+      for x in $buildInputs; do printf "  "; echo $x | cut -d "-" -f "2-"; done
+      echo "propagatedBuildInputs:"
       for x in $propagatedBuildInputs; do printf "  "; echo $x | cut -d "-" -f "2-"; done
-      echo "you can pass option '--argstr coq-version \"x.y\"' to nix-shell to change coq versions"
-      echo "you can pass option '--argstr mc-version \"x.y.z\"' to nix-shell to change mathcomp versions"
+      echo "you can pass option --arg config '{coq = \"x.y\"; math-comp = \"x.y.z\";}' to nix-shell to change coq and/or math-comp versions"
     }
     cachixEnv () {
       echo "Pushing environement to cachix"
+      for x in $buildInputs; do printf "  "; echo $x | cachix push math-comp; done
       for x in $propagatedBuildInputs; do printf "  "; echo $x | cachix push math-comp; done
     }
     nixDefault () {
@@ -66,9 +68,14 @@ let
   emacs = with pkgs; emacsWithPackages
     (epkgs: with epkgs.melpaStablePackages; [proof-general]);
 
-  package = with pkgs; (coqPackages.mathcomp-extra name src);
+  pkg = with pkgs;
+    if elem package
+      [ "mathcomp" "ssreflect" "mathcomp-ssreflect" "mathcomp-fingroup"
+        "mathcomp-algebra" "mathcomp-solvable" "mathcomp-field" "mathcomp-character" ]
+    then coqPackages.${package}
+    else (coqPackages.mathcomp-extra package src);
 in
-if pkgs.lib.trivial.inNixShell then package.overrideAttrs (old: {
+if pkgs.lib.trivial.inNixShell then pkg.overrideAttrs (old: {
   inherit shellHook mathcompnix;
   buildInputs = (old.buildInputs or []) ++ pkgs.lib.optional withEmacs emacs;
-}) else package
+}) else pkg
