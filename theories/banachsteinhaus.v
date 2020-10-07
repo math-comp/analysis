309f5131169a15542e29d9e55a0ee23a2bb67862
mkerjean <kerjean@irif.fr>
2020-10-07 15:29:32 +0200

    format

--- theories/banachsteinhaus.v
+++ theories/banachsteinhaus.v
@@ -122,12 +122,12 @@ have Ar: forall na : nat * (U * {posnum K}), exists b : (U * {posnum K}) , P (S(
   have majr : rn1b > 0 by apply min_pos_gt0.
   pose rn1 := PosNum majr.
   exists (an1,rn1); split.
-   - have temp: closed_ball an1 rn1b `<=` closed_ball an1 rn01%:num
-     by apply : closed_ball_ler; apply: minr_le2.
-     by apply: (subset_trans temp).
-   - apply: (@le_lt_trans _ _ (n.+3%:R^-1) rn1b (n.+2%:R^-1)).
-     by apply: minr_le1.
-     by rewrite ltf_pinv ?ltr_nat  ?posrE //=.
+    have temp: closed_ball an1 rn1b `<=` closed_ball an1 rn01%:num
+      by apply : closed_ball_ler; apply: minr_le2.
+    by apply: (subset_trans temp).
+  apply: (@le_lt_trans _ _ (n.+3%:R^-1) rn1b (n.+2%:R^-1)).
+    by apply: minr_le1.
+  by rewrite ltf_pinv ?ltr_nat  ?posrE //=.
 move: (choice Ar) => [f Pf].
 pose fix ar (n: nat):= match n with
                      | 0 => (a0,r0)
@@ -183,31 +183,31 @@ have : cvg (a @ \oo) by apply: cauchy_cvg.
 rewrite cvg_ex //=.
 move=> [l Hl] {Hf Dy OpenD H cauchya cauchyexa}.
 exists l; split.
-- have Hinter : (closed_ball a0 r0%:num) l.
+  have Hinter : (closed_ball a0 r0%:num) l.
   apply: (@closed_cvg _ _ \oo eventually_filter a); first by [].
   move=> m.
   have temp:  (0 <= m)%N by apply: leq0n.
     move : (Suite_ball 0%N m temp).
     have : closed_ball (a m) (r m)%:num (a m) by apply: closed_ballxx.
   by move=> temp1 Ha; move : (Ha (a m) temp1).
- by apply: closed_closed_ball_.
- have : closed_ball a0 r0%:num `<=` D.
+  by apply: closed_closed_ball_.
+  have : closed_ball a0 r0%:num `<=` D.
    by move: Ball_a0; rewrite closure_closed_ball subsetI; apply:  proj1.
-by move=>  Htemp; move : (Htemp l Hinter).
-- move=> i _.
-  have : closed_ball (a i) (r i)%:num l.
-    rewrite -(@cvg_shiftn i _ a l) in Hl.
-    simpl in Hl.
-    have partiecvg:  forall n : nat, closed_ball (a i) (r i)%:num (a (n + i)%N).
-      move=> n; apply: (Suite_ball i (n +i)%N); first by apply: leq_addl.
-      by apply: closed_ballxx.
-    apply: (@closed_cvg _ _ \oo eventually_filter (fun n : nat => a (n+i)%N)) => //=.
-    by apply:  closed_closed_ball_.
-  case i.
-  - by rewrite subsetI in Ball_a0; move: Ball_a0; move=> [_ p] la0; move: (p l la0).
-  - move=> n.
-    have [temp _] : P n.+1 (a n, r n) (a n.+1, r n.+1) by apply : (Pf (n , ar n)).
-    by rewrite subsetI in temp; move : temp; move=> [_ p] lan1; move: (p l lan1).
+  by move=>  Htemp; move : (Htemp l Hinter).
+move=> i _.
+have : closed_ball (a i) (r i)%:num l.
+rewrite -(@cvg_shiftn i _ a l) in Hl.
+simpl in Hl.
+have partiecvg:  forall n : nat, closed_ball (a i) (r i)%:num (a (n + i)%N).
+  move=> n; apply: (Suite_ball i (n +i)%N); first by apply: leq_addl.
+  by apply: closed_ballxx.
+apply: (@closed_cvg _ _ \oo eventually_filter (fun n : nat => a (n+i)%N)) => //=.
+  by apply:  closed_closed_ball_.
+case i.
+   by rewrite subsetI in Ball_a0; move: Ball_a0; move=> [_ p] la0; move: (p l la0).
+move=> n.
+have [temp _] : P n.+1 (a n, r n) (a n.+1, r n.+1) by apply : (Pf (n , ar n)).
+by rewrite subsetI in temp; move : temp; move=> [_ p] lan1; move: (p l lan1).
 Qed.
 
 End Baire.
@@ -219,22 +219,22 @@ Variable (V: completeNormedModType K)  (W: normedModType K).
 Lemma bounded_landau (f :{linear V->W}) : (* imp to define in normedtype.v *)
   bounded_fun_norm f <-> ((f : V -> W) =O_ (0:V) cst (1 : K^o)).
 Proof.
-  split.
-  - rewrite eqOP => bf.
-    move: (bf 1) => [M bm].
-    rewrite !nearE /=; exists M; split. by  apply : num_real.
-    move => x Mx; rewrite nearE nbhs_normP /=.
-    exists 1; first by [].
-    move => y /=. rewrite -ball_normE /ball_ sub0r normrN /cst normr1 mulr1 => y1.
-    apply: (@le_trans _ _ M _ _).
-    apply: (bm y); by apply: ltW.
-    by apply: ltW.
-  - rewrite eqOP /= ; move => Bf; apply/bounded_funP; rewrite /bounded_on.
-    near=>M.
-    set P :=  (X in (nbhs _ X)).
-    have -> : P  = (fun x : V => (`|f x| <= M * `|cst 1%R x|)%O).
-      by apply: funext => x; rewrite /cst normr1 mulr1.
-    by near: M.
+split.
+  rewrite eqOP => bf.
+  move: (bf 1) => [M bm].
+  rewrite !nearE /=; exists M; split. by  apply : num_real.
+  move => x Mx; rewrite nearE nbhs_normP /=.
+  exists 1; first by [].
+  move => y /=. rewrite -ball_normE /ball_ sub0r normrN /cst normr1 mulr1 => y1.
+  apply: (@le_trans _ _ M _ _).
+  apply: (bm y); by apply: ltW.
+  by apply: ltW.
+ rewrite eqOP /= ; move => Bf; apply/bounded_funP; rewrite /bounded_on.
+ near=>M.
+ set P :=  (X in (nbhs _ X)).
+ have -> : P  = (fun x : V => (`|f x| <= M * `|cst 1%R x|)%O).
+   by apply: funext => x; rewrite /cst normr1 mulr1.
+ by near: M.
 Grab Existential Variables. all: end_near. Qed.
 
 
@@ -258,103 +258,94 @@ Theorem Banach_Steinhauss (F: set ((V -> W))):
   (forall f, (F f) -> bounded_fun_norm f /\ linear f) ->
   pointwise_bounded F -> uniform_bounded F.
 Proof.
-  move=> Propf.
-  move=> BoundedF.
-  set O := (fun n :nat => \bigcup_(f in F) ((normr \o f)@^-1` [set y | y > n%:R])).
-  have O_open: forall n : nat, open ( O n ).
-   - move=>n.
-     apply: open_bigU.
-     move=> i App.
-     apply: (@open_comp _ _ ((normr : W -> K^o) \o i) [set y | y > n%:R]).
-     have Ci : continuous i.
-     + have Li : linear i by apply Propf.
-       have Bi : bounded_fun_norm i by apply Propf.
-       have Landaui : i =O_ (0:V) cst (1:K^o) by apply (@bounded_landau (Linear Li)).
-       by apply: (@linear_continuous K V W (Linear Li)).
-     move=> x Hx ; apply: continuous_comp.
-       + by apply: Ci.
-       + by apply: norm_continuous.
-      by apply: open_gt.
-  set O_inf := (\bigcap_i ( O i)).
-  have  O_infempty : O_inf = set0.
-     rewrite -subset0 => x //=.
-     move: (BoundedF x) => [M HMx].
-     rewrite /O_inf  /O //=  /bigsetI /bigsetU //=.
-     case  /(_ (`|Rtoint (floor M + 1%:~R)|%N)).
-     -  by rewrite /setT.
-     - move=> f  Hf abs; move : (HMx f Hf) => abs2.
-     have: (`|Rtoint (floor M + 1%:~R)|%:R < M) by  apply: (@lt_le_trans _ _ (`|f x|)).
-     have : (M < `|Rtoint(floor M + 1%:~R)|%:R)
-       by apply : floor_nat_comp; apply: (@le_trans _ _ `|f x|).
-     by apply : lt_nsym.
-  have BaireV : Baire V by apply : DeBaire.
-  have ContraBaire : exists i : nat, not (dense (O i)).
-   - unfold Baire in BaireV.
-     have BaireO : (forall i : nat, open(O i) /\ dense (O i)) -> dense (O_inf) by apply (BaireV O).
-     apply contrap  in BaireO.
-     + move: BaireO => /asboolPn /existsp_asboolPn [n /and_asboolP /nandP Hn].
-       by exists n ; case : Hn => /asboolPn.
-    + rewrite /dense O_infempty ; apply /existsNP.
-       exists setT. elim.
-       * by move=> x; rewrite setI0.
-       * by exists point.
-       * by apply: openT.
+move=> Propf BoundedF.
+set O := (fun n :nat => \bigcup_(f in F) ((normr \o f)@^-1` [set y | y > n%:R])).
+have O_open: forall n : nat, open ( O n ).
+  move=>n.
+  apply: open_bigU => i App.
+  apply: (@open_comp _ _ ((normr : W -> K^o) \o i) [set y | y > n%:R]).
+  have Ci : continuous i.
+    have Li : linear i by apply Propf.
+    have Bi : bounded_fun_norm i by apply Propf.
+    have Landaui : i =O_ (0:V) cst (1:K^o) by apply (@bounded_landau (Linear Li)).
+    by apply: (@linear_continuous K V W (Linear Li)).
+  move=> x Hx ; apply: continuous_comp; first by apply: Ci.
+  by apply: norm_continuous.
+  by apply: open_gt.
+set O_inf := (\bigcap_i ( O i)).
+have  O_infempty : O_inf = set0.
+rewrite -subset0 => x //=.
+move: (BoundedF x) => [M HMx];  rewrite /O_inf  /O //=  /bigsetI /bigsetU //=.
+case  /(_ (`|Rtoint (floor M + 1%:~R)|%N)); first by rewrite /setT.
+move=> f  Hf abs; move : (HMx f Hf) => abs2.
+have: (`|Rtoint (floor M + 1%:~R)|%:R < M) by  apply: (@lt_le_trans _ _ (`|f x|)).
+have : (M < `|Rtoint(floor M + 1%:~R)|%:R)
+  by apply : floor_nat_comp; apply: (@le_trans _ _ `|f x|).
+by apply : lt_nsym.
+have BaireV : Baire V by apply : DeBaire.
+have ContraBaire : exists i : nat, not (dense (O i)).
+  unfold Baire in BaireV.
+  have BaireO : (forall i : nat, open(O i) /\ dense (O i)) -> dense (O_inf) by apply (BaireV O).
+  apply contrap  in BaireO.
+    move: BaireO => /asboolPn /existsp_asboolPn [n /and_asboolP /nandP Hn].
+    by exists n ; case : Hn => /asboolPn.
+  rewrite /dense O_infempty ; apply /existsNP;exists setT; elim.
+  * by move=> x; rewrite setI0.
+  * by exists point.
+  * by apply: openT.
   have BaireContra : exists n :nat , exists x : V,
-               exists r : {posnum K}, (ball x r%:num) `<=` (~` (O n)).
-    - move: ContraBaire =>
-      [i /(denseNE) [ O0 [ [ x /open_nbhs_nbhs /nbhs_ballP [r [r0 bxr]]
-      /((@subsetI_eq0 _ (ball x r) O0 (O i) (O i)))]]]] H2.
-       by exists i; exists x; exists (PosNum r0) ;apply: setIsubset; apply H2.
-  move: BaireContra => [n [x0 [ r H ] ] k]; exists ((n%:R + n%:R) * k * 2 /r%:num); move=> f Hf y Hx.
-  move: (Propf f Hf) => [ _ linf].
-  case: (eqVneq y 0) => [-> | Zeroy]; last first.
-  - have  majballi : forall f, forall x, F f -> (ball x0 r%:num) x -> (`|f x | <= n%:R)%O.
-    move=> g x Fg Bx; move: (H x Bx).
-    rewrite /O //= /bigsetU //= /setC exists2P -forallNP.
-    move /(_  g).  case /not_and_or ; first by [].
-    by move=> /(@negP ((n%:R < `|g x|)%O)); rewrite -leNgt .
-    have majball : forall f, forall x, F f -> (ball x0 r%:num) x -> (`|f (x - x0) | <= (n%:R + n%:R))%O.
-    move=> g x Fg; move: (Propf g Fg) => [Bg Lg].
-    have Ling : g(x - x0) = g(x) - g(x0) by apply: (@linearsub x x0 g).
-    rewrite Ling.
-    move=> Ballx.
-    move: (majballi g x Fg Ballx) => Majx.
-    move: (majballi g x0 Fg   (ball_center x0 r)) => Majx0.
-    have Majf : `| g x | + `|g x0| <= n%:R + n%:R
-      by apply: (@ler_add _ `|g x| n%:R `|g x0| n%:R).
-    apply: (@le_trans _ _ (`|g x| + `|g x0|) (`|g x - g x0|) (n%:R + n%:R)).
-    - by apply: ler_norm_sub.
-    - by [].
-    have ballprop : ball x0 r%:num (2^-1  * (r%:num / `|y|) *: y  + x0).
-      rewrite -ball_normE /ball_ opprD.
-      rewrite addrC -addrA (@addrC _ (-x0) x0) addrN addr0 normrN normmZ.
-      rewrite R_normZ R_normZ -mulrA -mulrA  -(@normr_id _ _ y) -R_normZ normr_id.
-      rewrite /GRing.scale //= mulVf; last by rewrite normr_eq0.
-      rewrite normr1  mulr1 gtr0_norm; last by rewrite invr_gt0.
-      by rewrite gtr0_norm //=  gtr_pmull //= invf_lt1 //= ltr1n.
-    move: (majball f (2^-1 * (r%:num/`|y|)*:y + x0) Hf ballprop).
-    rewrite -addrA addrN linf.
-    have -> : f 0 =0 by rewrite -(linear0  (Linear linf)).
-    rewrite addr0 normmZ !R_normZ -ler_pdivl_mull //=.
-    rewrite !gtr0_norm //= ; last by rewrite invr_gt0 normr_gt0.
-    rewrite mulrA mulrC invf_div mulrA (@mulrC _ (2^-1) _) invf_div mulrA.
-    move=> Currentmaj {Propf BoundedF O O_open O_inf O_infempty BaireV ContraBaire H majball majballi}.
-    rewrite (@le_trans  _ _ ((n%:R + n%:R) * `|y| * 2 / r%:num)) //= => {Currentmaj}.    
-    rewrite (mulrC (n%:R + n%:R)) -ler_pdivl_mulr //=.
-    rewrite invrK -(mulrC r%:num) -(mulrC r%:num^-1) (mulrA r%:num) mulfV //=.
-    rewrite (mulrC 1) mulr1 -ler_pdivl_mulr //=.
-    rewrite -(mulrC 2) -(mulrC 2^-1) (mulrA 2^-1) mulVf //= (mulrC 1) mulr1.
-    case: n.
-    - by rewrite addr0 mulr0 (mulrC 0) mulr0.
-    - move => n.
-      rewrite -ler_pdivl_mulr //= -mulrC mulrA mulVf //=.
-      by rewrite (mulrC 1) mulr1.
-    rewrite mulr_gt0 ?invr_gt0 ?normr_gt0 //=.
-    rewrite mulr_gt0 ?invr_gt0 ?normr_gt0 //=.
-    by rewrite invr_neq0; rewrite ?normrE.
-    have -> : f 0 =0 by rewrite -(linear0  (Linear linf)).
-    rewrite normr0 !mulr_ge0 //=.
-    by rewrite (@le_trans _ _ `|y| _ _).
+             exists r : {posnum K}, (ball x r%:num) `<=` (~` (O n)).
+  move: ContraBaire =>
+  [i /(denseNE) [ O0 [ [ x /open_nbhs_nbhs /nbhs_ballP [r [r0 bxr]]
+   /((@subsetI_eq0 _ (ball x r) O0 (O i) (O i)))]]]] H2.
+   by exists i; exists x; exists (PosNum r0) ;apply: setIsubset; apply H2.
+move: BaireContra=> [n [x0 [ r H ] ] k]; exists ((n%:R + n%:R) * k * 2 /r%:num).
+move=> f Hf y Hx; move: (Propf f Hf) => [ _ linf]. 
+case: (eqVneq y 0) => [-> | Zeroy].
+  have -> : f 0 =0 by rewrite -(linear0  (Linear linf)).
+  rewrite normr0 !mulr_ge0 //=.
+  by rewrite (@le_trans _ _ `|y| _ _).
+have  majballi : forall f, forall x, F f -> (ball x0 r%:num) x -> (`|f x | <= n%:R)%O.
+move=> g x Fg Bx; move: (H x Bx).
+rewrite /O //= /bigsetU //= /setC exists2P -forallNP.
+move /(_  g).  case /not_and_or ; first by [].
+by move=> /(@negP ((n%:R < `|g x|)%O)); rewrite -leNgt .
+have majball : forall f, forall x, F f -> (ball x0 r%:num) x -> (`|f (x - x0) | <= (n%:R + n%:R))%O.
+move=> g x Fg; move: (Propf g Fg) => [Bg Lg].
+have Ling : g(x - x0) = g(x) - g(x0) by apply: (@linearsub x x0 g).
+rewrite Ling => Ballx.
+move: (majballi g x Fg Ballx) => Majx.
+move: (majballi g x0 Fg   (ball_center x0 r)) => Majx0.
+have Majf : `| g x | + `|g x0| <= n%:R + n%:R
+  by apply: (@ler_add _ `|g x| n%:R `|g x0| n%:R).
+apply: (@le_trans _ _ (`|g x| + `|g x0|) (`|g x - g x0|) (n%:R + n%:R)).
+  by apply: ler_norm_sub.
+by [].
+have ballprop : ball x0 r%:num (2^-1  * (r%:num / `|y|) *: y  + x0).
+  rewrite -ball_normE /ball_ opprD.
+  rewrite addrC -addrA (@addrC _ (-x0) x0) addrN addr0 normrN normmZ.
+  rewrite R_normZ R_normZ -mulrA -mulrA  -(@normr_id _ _ y) -R_normZ normr_id.
+  rewrite /GRing.scale //= mulVf; last by rewrite normr_eq0.
+  rewrite normr1  mulr1 gtr0_norm; last by rewrite invr_gt0.
+  by rewrite gtr0_norm //=  gtr_pmull //= invf_lt1 //= ltr1n.
+move: (majball f (2^-1 * (r%:num/`|y|)*:y + x0) Hf ballprop).
+rewrite -addrA addrN linf.
+have -> : f 0 =0 by rewrite -(linear0  (Linear linf)).
+rewrite addr0 normmZ !R_normZ -ler_pdivl_mull //=.
+rewrite !gtr0_norm //= ; last by rewrite invr_gt0 normr_gt0.
+rewrite mulrA mulrC invf_div mulrA (@mulrC _ (2^-1) _) invf_div mulrA.
+move=> Currentmaj {Propf BoundedF O O_open O_inf O_infempty BaireV ContraBaire H majball majballi}.
+rewrite (@le_trans  _ _ ((n%:R + n%:R) * `|y| * 2 / r%:num)) //= => {Currentmaj}.    
+rewrite (mulrC (n%:R + n%:R)) -ler_pdivl_mulr //=.
+rewrite invrK -(mulrC r%:num) -(mulrC r%:num^-1) (mulrA r%:num) mulfV //=.
+rewrite (mulrC 1) mulr1 -ler_pdivl_mulr //=.
+rewrite -(mulrC 2) -(mulrC 2^-1) (mulrA 2^-1) mulVf //= (mulrC 1) mulr1.
+case: n; first by rewrite addr0 mulr0 (mulrC 0) mulr0.
+move=> n; rewrite -ler_pdivl_mulr //= -mulrC mulrA mulVf //=.
+by rewrite (mulrC 1) mulr1.
+rewrite mulr_gt0 ?invr_gt0 ?normr_gt0 //=.
+rewrite mulr_gt0 ?invr_gt0 ?normr_gt0 //=.
+by rewrite invr_neq0; rewrite ?normrE.
 Qed.
 
 End banach_steinhauss.
