903c9adb2d61329a3b04b5ce9b88fb0a29c01f5f
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2020-11-25 01:04:34 +0900

    multiplication and absolute value for ereal
    - tentative

--- theories/ereal.v
+++ theories/ereal.v
@@ -275,6 +275,16 @@ Definition oppe x :=
   | -oo => +oo
   | +oo => -oo
   end.
+
+Definition mule x y :=
+  match x, y with
+  | x%:E , y%:E => (x * y)%:E
+  | -oo, y | y, -oo => if 0%:E <= y then -oo else +oo
+  | +oo, y | y, +oo => if 0%:E < y then +oo else -oo
+  end.
+
+Definition abse x := if x is r%:E then `|r|%:E else +oo.
+
 End ERealArith.
 
 Notation "+%E"   := adde.
@@ -282,6 +292,9 @@ Notation "-%E"   := oppe.
 Notation "x + y" := (adde x y) : ereal_scope.
 Notation "x - y" := (adde x (oppe y)) : ereal_scope.
 Notation "- x"   := (oppe x) : ereal_scope.
+Notation "*%E"   := mule.
+Notation "x * y" := (mule x y) : ereal_scope.
+Notation "`| x |" := (abse x) : ereal_scope.
 
 Notation "f \+ g" := (fun x => f x + g x)%E : ereal_scope.
 
@@ -864,11 +877,11 @@ case=> [x||].
       have [/eqP MQ0|MQ0] := boolP (MQ == 0).
         by exists 0; rewrite real0; split => // x x0; split;
         [apply/gtMP; rewrite MP0 | apply/gtMQ; rewrite MQ0].
-      exists `|MQ|; rewrite realE normr_ge0; split => // x Hx; split.
+      exists (`|MQ|)%R; rewrite realE normr_ge0; split => // x Hx; split.
         by apply gtMP; rewrite (le_lt_trans _ Hx) // MP0 lee_fin.
       by apply gtMQ; rewrite (le_lt_trans _ Hx) // lee_fin real_ler_normr // lexx.
     have [/eqP MQ0|MQ0] := boolP (MQ == 0).
-      exists `|MP|; rewrite realE normr_ge0; split => // x MPx; split.
+      exists (`|MP|)%R; rewrite realE normr_ge0; split => // x MPx; split.
       by apply gtMP; rewrite (le_lt_trans _ MPx) // lee_fin real_ler_normr // lexx.
       by apply gtMQ; rewrite (le_lt_trans _ MPx) // lee_fin MQ0.
     have {}MP0 : (0 < `|MP|)%R by rewrite normr_gt0.
