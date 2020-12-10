df43dcbc862075dbcea7538bf356c36a527d1845
Cyril Cohen <cohen@crans.org>
2020-12-10 17:30:12 +0100

    update meta.yml

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/archive/66e66b9d481386f4c7a6b998f32ce794af0182ae.tar.gz;
+    sha256 = "1zw3ib0705n7nskv9a2ipj1z0ys4wn2j8frnzhf1gx1yrgyjm8sn";
+  }
