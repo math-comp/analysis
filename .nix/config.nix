ba91a3ffcf60ae622c638933432d82119e57a787
Cyril Cohen <cohen@crans.org>
2020-12-10 15:25:21 +0100

    fixup

--- .nix/config.nix
+++ .nix/config.nix
@@ -1,11 +1,9 @@
 {
   pname = "mathcomp-analysis";
   overrides = {
-    coq = "8.11";
     coqPackages = {
+      coq = "8.11.2";
       mathcomp = "1.11.0";
-      mathcomp-real-closed = "1.1.1";
-      mathcomp-finmap = "1.5.0";
     };
   };
 }
