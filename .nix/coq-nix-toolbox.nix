ef8aca1a3a165414e704f86e425493cc12ae11fe
Cyril Cohen <cohen@crans.org>
2021-03-14 17:06:47 +0100

    missing file

---
+++ .nix/coq-nix-toolbox.nix
@@ -0,0 +1,5 @@
+# This file was generated from meta.yml, please do not edit manually.
+# Follow the instructions on https://github.com/coq-community/templates to regenerate.
+# However, this rev was not handled by mustache, but by a special eval command
+# because you picked the option nix-default: ... in meta.yml
+"de89ff16c149b7a7aec3cfe9d379641ee1aca229"
