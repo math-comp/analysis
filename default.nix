35d81388c0fc669a1cb61b1cf595c55649b63c6f
Cyril Cohen <cohen@crans.org>
2021-03-16 23:15:33 +0100

    up

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
 # This file was generated from `meta.yml`, please do not edit manually.
 # Follow the instructions on https://github.com/coq-community/templates to regenerate.
 { config ? {}, withEmacs ? false, print-env ? false, do-nothing ? false,
-  update-nixpkgs ? false, ci-matrix ? false, ci-job ? null,
+  update-nixpkgs ? false, ci-matrix ? false,
   override ? {}, ocaml-override ? {}, global-override ? {},
-  ci ? (!isNull ci-job), inNixShell ? null
+  task ? null, job ? null, inNixShell ? null, src ? ./.,
 }@args:
 let
   auto = fetchGit {
     url = "https://github.com/coq-community/coq-nix-toolbox.git";
-    ref = "master";
+    ref = "renaming-ci";
     rev = import ./.nix/coq-nix-toolbox.nix;
   };
 in
-(import auto ({src = ./.;} // args)).nix-auto
+(import auto ({inherit src;} // args)).nix-auto
 
