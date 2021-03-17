fe53d7b91b63113f7c117631a10a137718fcc398
Cyril Cohen <cohen@crans.org>
2021-03-17 03:05:16 +0100

    up

--- .nix/coq-nix-toolbox.nix
+++ .nix/coq-nix-toolbox.nix
@@ -2,4 +2,4 @@
 # Follow the instructions on https://github.com/coq-community/templates to regenerate.
 # However, this rev was not handled by mustache, but by a special eval command
 # because you picked the option nix-default: ... in meta.yml
-"da2ea25c53aeac9935c5b1f64074d860c13477b3"
+"868f7ed993f83f82388189257301f34e832933e0"
