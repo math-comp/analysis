0dfed45a1307331d328b44c220dde7420b76c3c9
zstone <zstonex@gmail.com>
2021-06-13 00:06:00 -0400

    pointwise compact images implies compact in ptws

--- theories/topology.v
+++ theories/topology.v
@@ -5333,125 +5333,44 @@ Definition uniformlyEquicontinuous {X V : uniformType} (W : set (X -> V)):=
 Section Evaluators.
 Context {X : choiceType}.
 Context {K : forall x:X, topologicalType}.
-Definition evaluator (x : X) (f : product_topologicalType K) := f x.
+Definition evaluator_dep (x : X) (f : product_topologicalType K) := f x.
 
-Lemma evaluatorE x f : evaluator x f = f x.
+Lemma evaluator_depE x f : evaluator_dep x f = f x.
 Proof. by []. Qed.
 
-Program Definition xval {x : X} (y : K x) i : K i := 
+Definition xval {x : X} (y : K x) i : K i := 
         match (pselect `[< x = i >]) return K i with 
-        | left r => _
+        | left r => ltac:(move/asboolP:r => <-; exact y)
         | right _ => point
         end.
-Next Obligation.
-  move/asboolP: r <-.
-  exact: y.
-Qed.
 
-Lemma evaluator_continuous x: continuous (evaluator x).
+Lemma evaluator_dep_continuous x: continuous (evaluator_dep x).
 Proof.
   move=> f.
-  have /cvg_sup/(_ x)/cvg_image : (f --> f) by admit.
+  have /cvg_sup/(_ x)/cvg_image : (f --> f) by apply: cvg_id.
   set P := ( x in (x -> _) -> _).
   have P' : P. {
     rewrite /P eqEsubset; split => y /=.
     - by [].
     - move=> _. exists (xval y) => //.
       rewrite /xval.
-      Search `[< _ = _ >].
-             
-
+      case (pselect _).
+      + move/asboolP => Q.
+        rewrite -Eqdep_dec.eq_rect_eq_dec => //.
+        move=> p q .
+        generalize (p = q).
+        exact: pselect.
+      + by move => Q; contradict Q; apply/ asboolP.
   } 
   move/(_ P') => Ff.
   apply: cvg_trans; last exact: Ff.
-  rewrite /= /evaluator=> Q /=. rewrite {1}/nbhs /=.
-  move=> [W nbdW <-].
-  rewrite /nbhs /=.
+  rewrite /= /evaluator_dep=> Q /=; rewrite {1}/nbhs /=.
+  move=> [W nbdW <-]; rewrite /nbhs /=.
   apply: filterS; last exact: nbdW.
   by move=> g Wg /=; exists g. 
 Qed.
 
-
-  Search (nbhs (_ _)).
-
-
-  set F1 := (x in x --> _ -> _).
-  set F2 := (x in _ -> x --> _).
-
-  suff: (F1 `=>` F2). by applyl 
-  rewrite /F1/F2 eqEsubset /evaluator/=.
-  split=> Q /=.
-  - move => [W nbdA <- /=].
-    set W' := (x in nbhs _ x).
-    suff: W = W' by move=> <- //.
-    rewrite /W' eqEsubset; split => g /=.
-    + move=> ?; exists g; tauto.
-    + move=> [h Wh].
-  
-  rewrite  /evaluator.
-  move=> H Q fQ /=.
-
-
-
-
-  rewrite /evaluator.
-  rewrite /filter_of {1}/nbhs/=.
-  done.
-  (*
-  have := @weak_continuous 
-    (dep_arrow_pointedType K) (K x) (evaluator x) f.
-    *)
-  have : continuous (@id (product_topologicalType K)) by 
-      move=> ?; apply: cvg_id.
-  have ? : Filter (id @ f) by admit.
-  have : (f --> (f : (dep_arrow_pointedType (fun x : X => K x)))).
-  move/(_ f)/cvg_sup.
-  rewrite /evaluator/cvg_to/filter_of.
-  apply.
-
-
-  
-
-
-  2: admit.
-
-
-    (dep_arrow_pointedType K) (K x) (evaluator x) f.
-  rewrite cvg_sup
-  
-  
-  move/continuousP/(_ A oA).
-  rewrite /product_topologicalType/evaluator.
-
-
-  rewrite /product_topologicalType/sup_topologicalType /=.
-  apply.
-  move => [P /= [[A oA AP] [Pf PsubQ]]]; subst.
-  eexists;eexists; last by split; eauto.
-  exists ([set (evaluator x @^-1` A)]); last exact: bigcup_set1.
-  rewrite /set1 /evaluator => U /= ->.
-  exists (fset1 [set t | A (t x)]) => /=.
-  - move=> R /fset1P ->; rewrite in_setE /=; exists x => //.
-    apply: open_comp  => /=.
-    move=> q qA.
-    admit.
-  - rewrite /bigsetI /= eqEsubset; split =>t /=.
-    + apply; apply: fset11.
-    + by move=> ? ? /fset1P -> //.
-
-
-
-  
-
-  ko: move => P /= ->; exists 
-  Search bigsetU.
-
-  exists [set fg | I (fg.1 x, fg.2 x) ] => //=. 
-  - exists [set xy | I (xy.1, xy.2)] => //=; last by (move=> ? //=).
-    under eq_fun do rewrite -surjective_pairing.
-    exact: eI.
-  - by move=> y /=; apply: IsubQ.
-Qed.
+End Evaluators.
 Section Precompact.
 
 Context {X : topologicalType}.
@@ -5465,16 +5384,17 @@ apply: (subclosed_compact (B := closure B)) => //.
 - exact: closed_closure.
 - exact: closure_subset.
 Qed.
-
-Definition pointwisePrecomact {V : topologicalType} (W : set (X -> V)):=
-  forall x, precompact [set (f x) | f in W].
   
 Context {V : uniformType}.
 Context {hsdf : hausdorff V}.
+
 Definition evaluator (x : X) (f : {unif, X -> V}) := f x.
 
 Lemma evaluatorE x f : evaluator x f = f x.
 Proof. by []. Qed.
+
+Definition pointwisePrecomact {V : topologicalType} (W : set (X -> V)):=
+  forall x, precompact [set (f x) | f in W].
 
 Lemma evaluator_continuous x: continuous (evaluator x).
 Proof.
@@ -5504,8 +5424,7 @@ Proof.
     + by rewrite closure_limit_point image_setU => ? ?; left.
 Qed.
 
-End Precompact.
-Lemma hausdorff_pointwise {X : choiceType} {K : forall x, topologicalType} : 
+Lemma hausdorff_pointwise {K : forall x, topologicalType} : 
   (forall x, hausdorff (K x)) ->
   hausdorff (@product_topologicalType X K).
 Proof.
@@ -5513,12 +5432,17 @@ Proof.
   apply: functional_extensionality_dep => x.
   apply: hsdfV.
   move: clstr; rewrite ?cluster_cvgE /= => [[G PG [GtoQ psubG]]].
-  set f := (fun (f : forall y, K y) => f x ).
-  exists (f @ G); first exact: fmap_proper_filter. 
-  
-  Search (ProperFilter ( _ @ _)).
-  apply: hsdfV => A B pxA qxB.
-  rewrite /cluster /= /meets /=.
+  exists (evaluator_dep x @ G); first exact: fmap_proper_filter; split.
+  - rewrite -(evaluator_depE x).
+    apply: cvg_trans; last apply: (@evaluator_dep_continuous _ _ x q).
+    apply: cvg_app; exact GtoQ.
+  - move/(cvg_app (evaluator_dep x)):psubG => xpsubxG.
+    apply: cvg_trans; first exact: xpsubxG.
+    apply: evaluator_dep_continuous.
+Qed.
+
+Hint Resolve hausdorff_pointwise: core.
+
 Lemma ArzelaAscoli_aux2  (W : set ({ptws, X -> V})):
   pointwisePrecomact W -> precompact W.
 Proof.
@@ -5531,26 +5455,17 @@ Proof.
     - apply: closed_closure.
     - exact: C.
     - set R := (x in _`<=` x).
-      have cR : closed R.
-        apply: compact_closed => //.
-      suff /closure_subset WsubR: W `<=` R.
-      apply: subset_trans; first exact:WsubR.
-
-      Search closure.
-       {
+      have WsubR : W `<=` R. {
         rewrite /R/K => f /= Wf x; rewrite closure_limit_point /=.
-        left => /=; exists f; tauto.
+        by left => /=; exists f; tauto.
       }
-      ]
-      
-      have := (continuous_image_closure (f:= evaluator x) (U := W) 
-          (ltac: (apply: evaluator_continuous))).
-      rewrite /evaluator => G clWf.
-      by apply: G => /=; exists f.
-    
-    rewrite {}/K; set D := (x in compact x) => cptD.
+      have cR : closed R by
+        by apply: compact_closed => //; apply: hausdorff_pointwise.
+      rewrite closureE /=.
+      move => q /=; apply; split => //.
+Qed.
 
-
+End Precompact.
 Section ArzelaAscoli.
 Context {X V : uniformType}.
 Context {hsdfV : hausdorff V}.
