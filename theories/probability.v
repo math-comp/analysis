41773f340be590c2db71dcb8e3e7dcd19a29c7dc
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2023-06-06 17:21:04 +0900

    rebase

--- theories/probability.v
+++ theories/probability.v
@@ -4,8 +4,9 @@ From mathcomp Require Import ssralg poly ssrnum ssrint interval finmap.
 From mathcomp Require Import mathcomp_extra boolp classical_sets functions.
 From mathcomp Require Import cardinality.
 From HB Require Import structures.
-Require Import reals ereal signed topology normedtype sequences esum measure.
-Require Import exp numfun lebesgue_measure lebesgue_integral.
+From mathcomp.classical Require Import functions cardinality.
+Require Import reals ereal signed topology normedtype sequences exp esum.
+Require Import measure numfun lebesgue_measure lebesgue_integral.
 
 (******************************************************************************)
 (*                       Probability (experimental)                           *)
