bad7138bddd78b7e8dec62443c5d0974a3ac8cea
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2023-06-24 21:40:55 +0900

    put all_classical in Make

--- classical/all_classical.v
+++ classical/all_classical.v
@@ -1,7 +1,7 @@
-Require Export boolp.
-Require Export classical_sets.
-Require Export mathcomp_extra.
-Require Export functions.
-Require Export cardinality.
-Require Export fsbigop.
-Require Export set_interval.
+From mathcomp.classical Require Export boolp.
+From mathcomp.classical Require Export classical_sets.
+From mathcomp.classical Require Export mathcomp_extra.
+From mathcomp.classical Require Export functions.
+From mathcomp.classical Require Export cardinality.
+From mathcomp.classical Require Export fsbigop.
+From mathcomp.classical Require Export set_interval.
