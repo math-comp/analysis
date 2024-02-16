dd95eabe8608bebd60b7e065d44155f34b6ef379
AyumuSaito <awe.nun02@gmail.com>
2024-02-16 10:48:05 +0900

    write casino (wip)
    - add exp_beta

--- theories/lang_syntax.v
+++ theories/lang_syntax.v
@@ -345,6 +345,75 @@ End accessor_functions.
 Arguments acc_typ {R} s n.
 Arguments measurable_acc_typ {R} s n.
 
+
+Section beta.
+Context {R : realType}.
+(* Check sample_cst (beta 6 4) : R.-sfker _ ~> R. *)
+(* Check sample_cst (beta 6 4) : R.-ker _ ~> measurableTypeR R. *)
+Check sample_cst (uniform_probability _) : R.-ker _ ~> measurableTypeR R.
+
+Open Scope ring_scope.
+Lemma a01 : 0 < 1 - 0 :> R. Proof. by []. Qed.
+Import Notations.
+
+(* Lemma beta11_uniform :
+  beta 1 1 `[0, 1] = uniform_probability a01 `[0, 1].
+Proof.
+rewrite /beta /uniform_probability.
+congr mscale.
+congr invr_nonneg.
+admit.
+rewrite /mscale.
+apply: funext=> x. *)
+
+Definition beta_bern : R.-sfker munit ~> mbool := 
+  @letin' _ _ _ munit _ mbool R
+  (sample_cst (beta 6 4))
+  (* (sample_cst (uniform_probability a01)) *)
+  (sample (bernoulli_trunc \o (@fst (measurableTypeR R) _)) (measurableT_comp measurable_bernoulli_trunc (measurable_acc_typ [:: Real] 0))).
+
+Lemma letin'_sample_uniform d d' (T : measurableType d)
+    (T' : measurableType d') (a b : R) (ab0 : (0 < b - a)%R)
+    (u : R.-sfker [the measurableType _ of (_ * T)%type] ~> T') x y :
+  measurable y ->
+  letin' (sample_cst (uniform_probability ab0)) u x y =
+  ((b - a)^-1%:E * \int[lebesgue_measure]_(x0 in `[a, b]) u (x0, x) y)%E.
+Admitted.
+
+Definition uni_bern : R.-sfker munit ~> mbool := 
+  @letin' _ _ _ munit (measurableTypeR R) mbool R
+  (sample_cst (uniform_probability a01))
+  (* (sample_cst (uniform_probability a01)) *)
+  (sample (bernoulli_trunc \o (@fst (measurableTypeR R) _)) (measurableT_comp measurable_bernoulli_trunc (measurable_acc_typ [:: Real] 0))).
+
+Lemma ex_beta_bern : beta_bern tt [set true] = (6 / 10)%:E.
+Proof.
+rewrite /beta_bern /uni_bern.
+rewrite [LHS]letin'E.
+rewrite /beta.
+rewrite ge0_integral_mscale//.
+rewrite /mscale/=/B.
+rewrite /prebeta/=.
+Search integral lebesgue_measure.
+
+(* rewrite ge0_integral_mscale//=.
+rewrite EFinM.
+congr (_ * _)%E.
+rewrite /prebeta/=.
+Search "pow".
+rewrite subn1/=.
+transitivity (\int[(integral (uniform_probability p01))^~ (@cst R _ 1%:E)]_x bernoulli_trunc x U)%E.
+  admit.
+rewrite /bernoulli_trunc/=.
+rewrite integral_bernoulli_trunc.
+(* rewrite /B invr1 !mulr1 fact0 invr1 mul1e. *)
+rewrite /prebeta/=.
+under eq_integral.
+Search integral lebesgue_measure. *)
+Abort.
+
+End beta.
+
 Section context.
 Variables (R : realType).
 Definition ctx := seq (string * typ).
@@ -450,7 +519,7 @@ Inductive exp : flag -> ctx -> typ -> Type :=
 | exp_binomial_trunc g (n : nat) :
     exp D g Real -> exp D g (Prob Nat)
 | exp_uniform g (a b : R) (ab0 : (0 < b - a)%R) : exp D g (Prob Real)
-| exp_beta g (a b : nat) : exp D g (Prob Real)
+| exp_beta g (a b : nat) (* NB: shound be R *) : exp D g (Prob Real)
 | exp_poisson g : nat -> exp D g Real -> exp D g Real
 | exp_normalize g t : exp P g t -> exp D g (Prob t)
 | exp_letin g t1 t2 str : exp P g t1 -> exp P ((str, t1) :: g) t2 ->
@@ -757,8 +826,8 @@ Inductive evalD : forall g t, exp D g t ->
   (exp_uniform a b ab0 : exp D g _) -D> cst (uniform_probability ab0) ;
                                         measurable_cst _
 
-| eval_beta g (a b : nat) (p : {nonneg R}) (p1 : (p%:num <= 1)%R) :
-  (exp_beta a b : exp D g _) -D> cst (beta a b p1) ; measurable_cst _
+| eval_beta g (a b : nat) :
+  (exp_beta a b : exp D g _) -D> cst (beta a b) ; measurable_cst _
 
 | eval_poisson g n (e : exp D g _) f mf :
   e -D> f ; mf ->
@@ -895,7 +964,7 @@ all: (rewrite {g t e u v mu mv hu}).
   by rewrite (IH _ _ H2).
 - move=> g n p p1 {}v {}mv.
   inversion 1; subst g0 n0 p0.
-  inj_ex H4; subst v.
+  inj_ex H2; subst v.
   by have -> : p1 = p3 by [].
 - move=> g n e f mf ev IH {}v {}mv.
   inversion 1; subst g0 n0.
@@ -905,17 +974,21 @@ all: (rewrite {g t e u v mu mv hu}).
   by rewrite (IH _ _ H3).
 - move=> g a b ab0 {}v {}mv.
   inversion 1; subst g0 a0 b0.
-  inj_ex H4; subst v.
-  by have -> : ab0 = ab2.
-- move=> g a b p p1 {}v {}mv.
-  inversion 1. subst g0 a0 b0.
   inj_ex H2; subst v.
-  inj_ex H4.
-  have -> : p1 = p2 by [].
-- move=> g t e0 k ev IH {}v {}mv.
-  inversion 1; subst g0 t0.
+  by have -> : ab0 = ab2.
+- move=> g a b {}v {}mv.
+  inversion 1. subst g0 a0 b0.
+  by inj_ex H2; subst v. (* TODO: beta *)
+- move=> g t e k mk ev IH {}v {}mv.
+  inversion 1; subst g0 t.
   inj_ex H2; subst e0.
   inj_ex H4; subst v.
+  by rewrite (IH _ _ H3).
+- move=> g t e k ev IH f mf.
+  inversion 1; subst g0 t0.
+  inj_ex H2; subst e0.
+  inj_ex H4; subst f.
+  inj_ex H5; subst mf.
   by rewrite (IH _ H3).
 - move=> g t e f mf e1 f1 mf1 e2 f2 mf2 ev ih ev1 ih1 ev2 ih2 v m.
   inversion 1; subst g0 t0.
@@ -1049,7 +1122,7 @@ all: rewrite {g t e u v eu}.
   by rewrite (IH _ _ H2).
 - move=> g n p p1 {}v {}mv.
   inversion 1; subst g0 n0 p0.
-  inj_ex H4; subst v.
+  inj_ex H2; subst v.
   by have -> : p1 = p3 by [].
 - move=> g n e f mf ev IH {}v {}mv.
   inversion 1; subst g0 n0.
@@ -1059,8 +1132,11 @@ all: rewrite {g t e u v eu}.
   by rewrite (IH _ _ H3).
 - move=> g a b ab0 {}v {}mv.
   inversion 1; subst g0 a0 b0.
-  inj_ex H4; subst v.
+  inj_ex H2; subst v.
   by have -> : ab0 = ab2.
+- move=> g a b {}v {}mv.
+  inversion 1; subst g0 a0 b0.
+  by inj_ex H2; subst v.
 - move=> g n e f mf ev IH {}v {}mv.
   inversion 1; subst g0 n0.
   inj_ex H2; subst e0.
@@ -1162,6 +1238,7 @@ all: rewrite {z g t}.
   exists (binomial_probability_trunc n \o p).
   eexists; exact: (eval_binomial_trunc n).
 - by eexists; eexists; exact: eval_uniform.
+- by eexists; eexists; exact: eval_beta.
 - move=> g h e [f [mf H]].
   by exists (poisson h \o f); eexists; exact: eval_poisson.
 - move=> g t e [k ek].
@@ -1356,6 +1433,11 @@ Lemma execD_uniform g a b ab0 :
   @execD g _ (exp_uniform a b ab0) =
     existT _ (cst [the probability _ _ of uniform_probability ab0]) (measurable_cst _).
 Proof. exact/execD_evalD/eval_uniform. Qed.
+
+Lemma execD_beta g a b :
+  @execD g _ (exp_beta a b) =
+    existT _ (cst [the probability _ _ of beta a b]) (measurable_cst _).
+Proof. exact/execD_evalD/eval_beta. Qed.
 
 Lemma execD_normalize_pt g t (e : exp P g t) :
   @execD g _ [Normalize e] =
