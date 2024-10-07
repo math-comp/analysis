e1981695f9273425a665751c863410778a15cee8
mkerjean <marie.kerjean@lipn.univ-paris13.fr>
2024-10-07 14:41:53 +0200

    misplaced hint

--- theories/separation_axioms.v
+++ theories/separation_axioms.v
@@ -258,6 +258,8 @@ Import numFieldTopology.Exports.
 Lemma Rhausdorff (R : realFieldType) : hausdorff_space R.
 Proof. exact: order_hausdorff. Qed.
 
+Hint Extern 0 (hausdorff_space _) => (exact: Rhausdorff ) : core.
+
 Section separated_topologicalType.
 Variable T : topologicalType.
 Implicit Types x y : T.
