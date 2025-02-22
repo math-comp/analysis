0b54d8f3279e2a57799afde2a6bba353cf0d4fb4
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2025-02-22 23:27:48 +0900

    use gauss_integral to complete another example

--- theories/prob_lang_wip.v
+++ theories/prob_lang_wip.v
@@ -6,7 +6,8 @@ From mathcomp Require Import functions cardinality fsbigop.
 From mathcomp Require Import interval_inference reals ereal topology normedtype.
 From mathcomp Require Import sequences esum measure lebesgue_measure numfun.
 From mathcomp Require Import lebesgue_integral exp kernel trigo prob_lang.
-From mathcomp Require Import realfun charge.
+From mathcomp Require Import realfun charge probability derive ftc.
+From mathcomp Require Import gauss_integral.
 
 (******************************************************************************)
 (*  Semantics of a probabilistic programming language using s-finite kernels  *)
@@ -23,175 +24,96 @@ Local Open Scope classical_set_scope.
 Local Open Scope ring_scope.
 Local Open Scope ereal_scope.
 
-Section gauss_pdf.
-Context {R : realType}.
-Local Open Scope ring_scope.
-
-Definition gauss_pdf m s x : R :=
-  (s * sqrtr (pi *+ 2))^-1 * expR (- ((x - m) / s) ^+ 2 / 2%:R).
-
-Lemma gauss_pdf_ge0 m s x : 0 <= s -> 0 <= gauss_pdf m s x.
-Proof. by move=> s0; rewrite mulr_ge0 ?expR_ge0// invr_ge0 mulr_ge0. Qed.
-
-Lemma gauss_pdf_gt0 m s x : 0 < s -> 0 < gauss_pdf m s x.
-Proof.
-move=> s0; rewrite mulr_gt0 ?expR_gt0// invr_gt0 mulr_gt0//.
-by rewrite sqrtr_gt0 pmulrn_rgt0// pi_gt0.
-Qed.
-
-Lemma measurable_gauss_pdf m s : measurable_fun setT (gauss_pdf m s).
-Proof.
-apply: measurable_funM => //=; apply: measurableT_comp => //=.
-apply: measurable_funM => //=; apply: measurableT_comp => //=.
-apply: measurableT_comp (exprn_measurable _) _ => /=.
-by apply: measurable_funM => //=; exact: measurable_funD.
-Qed.
-
-Definition gauss_pdf01 : R -> R := gauss_pdf 0 1.
-
-Lemma gauss_pdf01E x :
-  gauss_pdf01 x = (sqrtr (pi *+ 2))^-1 * expR (- (x ^+ 2) / 2%:R).
-Proof. by rewrite /gauss_pdf01 /gauss_pdf mul1r subr0 divr1. Qed.
-
-Lemma gauss_pdf01_ub x : gauss_pdf01 x <= (Num.sqrt (pi *+ 2))^-1.
-Proof.
-rewrite -[leRHS]mulr1.
-rewrite /gauss_pdf01 /gauss_pdf; last first.
-rewrite mul1r subr0 ler_pM2l ?invr_gt0// ?sqrtr_gt0; last by rewrite mulrn_wgt0// pi_gt0.
-by rewrite -[leRHS]expR0 ler_expR mulNr oppr_le0 mulr_ge0// sqr_ge0.
-Qed.
-
-Lemma continuous_gauss_pdf1 x : {for x, continuous gauss_pdf01}.
-Proof.
-apply: continuousM => //=; first exact: cvg_cst.
-apply: continuous_comp => /=; last exact: continuous_expR.
-apply: continuousM => //=; last exact: cvg_cst.
-apply: continuous_comp => //=; last exact: (@continuousN _ R^o).
-apply: (@continuous_comp _ _ _ _ (fun x : R => x ^+ 2)%R); last exact: exprn_continuous.
-apply: continuousM => //=; last exact: cvg_cst.
-by apply: (@continuousD _ R^o) => //=; exact: cvg_cst.
-Qed.
-
-End gauss_pdf.
-
-Definition gauss01 {R : realType}
-  of \int[@lebesgue_measure R]_x (gauss_pdf01 x)%:E = 1%E : set _ -> \bar R :=
-  fun V => (\int[lebesgue_measure]_(x in V) (gauss_pdf01 x)%:E)%E.
-
 Section gauss.
 Variable R : realType.
 Local Open Scope ring_scope.
 
-Hypothesis integral_gauss_pdf01 :
-  (\int[@lebesgue_measure R]_x (gauss_pdf01 x)%:E = 1%E)%E.
+Definition gauss_pdf := @normal_pdf R 0 1.
 
-Local Notation gauss01 := (gauss01 integral_gauss_pdf01).
-
-Let gauss010 : gauss01 set0 = 0%E.
-Proof. by rewrite /gauss01 integral_set0. Qed.
-
-Let gauss01_ge0 A : (0 <= gauss01 A)%E.
+(* TODO: move to probability.v *)
+Lemma normal_pdf_gt0 m s x : 0 < s -> 0 < normal_pdf m s x :> R.
 Proof.
-by rewrite /gauss01 integral_ge0//= => x _; rewrite lee_fin gauss_pdf_ge0.
+move=> s0; rewrite /normal_pdf gt_eqF// mulr_gt0 ?expR_gt0// invr_gt0.
+by rewrite sqrtr_gt0 pmulrn_rgt0// mulr_gt0 ?pi_gt0// exprn_gt0.
 Qed.
 
-Let gauss01_sigma_additive : semi_sigma_additive gauss01.
-Proof.
-move=> /= F mF tF mUF.
-rewrite /gauss01/= integral_bigcup//=; last first.
-  apply/integrableP; split.
-    apply/measurable_funTS/measurableT_comp => //.
-    exact: measurable_gauss_pdf.
-  rewrite (_ : (fun x => _) = EFin \o gauss_pdf01); last first.
-    by apply/funext => x; rewrite gee0_abs// lee_fin gauss_pdf_ge0.
-  apply: le_lt_trans.
-    apply: (@ge0_subset_integral _ _ _ _ _ setT) => //=.
-      by apply/measurable_EFinP; exact: measurable_gauss_pdf.
-    by move=> ? _; rewrite lee_fin gauss_pdf_ge0.
-  by rewrite integral_gauss_pdf01 // ltey.
-apply: is_cvg_ereal_nneg_natsum_cond => n _ _.
-by apply: integral_ge0 => /= x ?; rewrite lee_fin gauss_pdf_ge0.
-Qed.
+Lemma gauss_pdf_gt0 x : 0 < gauss_pdf x.
+Proof. exact: normal_pdf_gt0. Qed.
 
-HB.instance Definition _ := isMeasure.Build _ _ _
-  gauss01 gauss010 gauss01_ge0 gauss01_sigma_additive.
+Definition gauss_prob := @normal_prob R 0 1.
 
-Let gauss01_setT : gauss01 [set: _] = 1%E.
-Proof. by rewrite /gauss01 integral_gauss_pdf01. Qed.
+HB.instance Definition _ := Probability.on gauss_prob.
 
-HB.instance Definition _ := @Measure_isProbability.Build _ _ R gauss01 gauss01_setT.
+Lemma gauss_prob_dominates : gauss_prob `<< lebesgue_measure.
+Proof. exact: normal_prob_dominates. Qed.
+
+Lemma continuous_gauss_pdf x : {for x, continuous gauss_pdf}.
+Proof. exact: continuous_normal_pdf. Qed.
 
 End gauss.
 
 Section gauss_lebesgue.
 Context d (T : measurableType d) (R : realType).
 Notation mu := (@lebesgue_measure R).
-Hypothesis integral_gauss_pdf01 : \int[mu]_x (gauss_pdf01 x)%:E = 1%E.
 
-Lemma gauss01_dom : gauss01 integral_gauss_pdf01 `<< mu.
+Let f1 (x : g_sigma_algebraType (R.-ocitv.-measurable)) := (gauss_pdf x)^-1.
+
+Let f1E (x : R) : f1 x = (Num.sqrt (pi *+ 2) * expR (- (- x ^+ 2 / 2)))%R.
 Proof.
-move=> A mA muA0; rewrite /gauss01.
-apply/eqP; rewrite eq_le; apply/andP; split; last first.
-  by apply: integral_ge0 => x _; rewrite lee_fin gauss_pdf_ge0.
-apply: (@le_trans _ _ (\int[mu]_(x in A) (Num.sqrt (pi *+ 2))^-1%:E))%E; last first.
-  by rewrite integral_cst//= muA0 mule0.
-apply: ge0_le_integral => //=.
-- by move=> x _; rewrite lee_fin gauss_pdf_ge0.
-- apply/measurable_funTS/measurableT_comp => //.
-  exact: measurable_gauss_pdf.
-- by move=> x _; rewrite lee_fin gauss_pdf01_ub.
+rewrite /f1 /gauss_pdf /normal_pdf oner_eq0 subr0 expr1n mul1r.
+by rewrite invfM invrK -expRN.
 Qed.
 
-Let f1 (x : g_sigma_algebraType (R.-ocitv.-measurable)) := (gauss_pdf01 x) ^-1.
+Let f1_gt0 (x : R) : (0 < f1 x)%R.
+Proof. by rewrite f1E mulr_gt0 ?expR_gt0// sqrtr_gt0 mulrn_wgt0// pi_gt0. Qed.
 
 Lemma measurable_fun_f1 : measurable_fun setT f1.
 Proof.
 apply: continuous_measurable_fun => x.
-apply: (@continuousV _ _ gauss_pdf01).
+apply: (@continuousV _ _ (@gauss_pdf R)).
   by rewrite gt_eqF// gauss_pdf_gt0.
-exact: continuous_gauss_pdf1.
+exact: continuous_gauss_pdf.
 Qed.
 
-Lemma integrable_f1 U : measurable U ->
-  (gauss01 integral_gauss_pdf01).-integrable U (fun x : g_sigma_algebraType (R.-ocitv.-measurable) => (f1 x)%:E).
-Proof.
-Admitted.
-
 Lemma integral_mgauss01 : forall U, measurable U ->
-  \int[gauss01 integral_gauss_pdf01]_(y in U) (f1 y)%:E =
-  \int[mu]_(x0 in U) (gauss_pdf01 x0 * f1 x0)%:E.
+  \int[(@gauss_prob R)]_(y in U) (f1 y)%:E =
+  \int[mu]_(x0 in U) (gauss_pdf x0 * f1 x0)%:E.
 Proof.
 move=> U mU.
 under [in RHS]eq_integral do rewrite EFinM/= muleC.
-rewrite -(Radon_Nikodym_change_of_variables gauss01_dom _ (integrable_f1 mU))//=.
+rewrite /=.
+rewrite -(@Radon_Nikodym_SigmaFinite.change_of_variables
+    _ _ _ _ (@lebesgue_measure R))//=; last 3 first.
+  exact: gauss_prob_dominates.
+  by move=> /= x; rewrite lee_fin ltW.
+  apply/measurable_EFinP.
+  apply: measurable_funTS.
+  exact: measurable_fun_f1.
 apply: ae_eq_integral => //=.
 - apply: emeasurable_funM => //.
     apply/measurable_funTS/measurableT_comp => //.
     exact: measurable_fun_f1.
   apply: (measurable_int mu).
   apply: (integrableS _ _ (@subsetT _ _)) => //=.
-  apply: Radon_Nikodym_integrable => //=.
-  exact: gauss01_dom.
+  apply: Radon_Nikodym_SigmaFinite.f_integrable => /=.
+  exact: gauss_prob_dominates.
 - apply: emeasurable_funM => //.
     apply/measurable_funTS/measurableT_comp => //.
     exact: measurable_fun_f1.
   apply/measurable_funTS/measurableT_comp => //.
-  exact: measurable_gauss_pdf.
+  exact: measurable_normal_pdf.
 - apply: ae_eq_mul2l => /=.
-  rewrite Radon_NikodymE//=.
-    exact: gauss01_dom.
-  move=> gauss01_dom'.
-  case: cid => //= h [h1 h2 h3].
+  rewrite /Radon_Nikodym_SigmaFinite.f/=.
+  case: pselect => [gauss_prob_dom|]; last first.
+    by move=> /(_ (@gauss_prob_dominates R)).
+  case: cid => //= h [h1 h2 h3] gauss_probE.
   apply: integral_ae_eq => //=.
-  + exact: integrableS h2.
+  + exact: integrableS h3.
   + apply/measurable_funTS/measurableT_comp => //.
-    exact: measurable_gauss_pdf.
-  + by move=> E EU mE; rewrite -(h3 _ mE).
+    exact: measurable_normal_pdf.
+  + move=> E EU mE.
+    by rewrite -gauss_probE.
 Qed.
-
-(*Hypothesis integral_mgauss01 : forall U, measurable U ->
-  \int[gauss01 integral_gauss_pdf01]_(y in U) (f1 y)%:E =
-  \int[mu]_(x0 in U) (gauss_pdf01 x0 * f1 x0)%:E.*)
 
 Let mf1 : measurable_fun setT f1.
 Proof.
@@ -200,11 +122,11 @@ apply: (measurable_comp (F := [set r : R | r != 0%R])) => //.
 - by move=> /= r [t _ <-]; rewrite gt_eqF// gauss_pdf_gt0.
 - apply: open_continuous_measurable_fun => //.
   by apply/in_setP => x /= x0; exact: inv_continuous.
-- exact: measurable_gauss_pdf.
+- exact: measurable_normal_pdf.
 Qed.
 
 Definition staton_lebesgue : R.-sfker T ~> _ :=
-  letin (sample_cst (gauss01 integral_gauss_pdf01 : pprobability _ _))
+  letin (sample_cst (@gauss_prob R : pprobability _ _))
   (letin
     (score (measurableT_comp mf1 macc1of2))
     (ret macc1of3)).
@@ -214,11 +136,11 @@ Lemma staton_lebesgueE x U : measurable U ->
 Proof.
 move=> mU; rewrite [in LHS]/staton_lebesgue/=.
 rewrite [in LHS]letinE /=.
-transitivity (\int[gauss01 integral_gauss_pdf01]_(y in U) (f1 y)%:E).
+transitivity (\int[(@gauss_prob R)]_(y in U) (f1 y)%:E).
   rewrite -[in RHS](setTI U) integral_mkcondr/=.
   apply: eq_integral => //= r _.
   rewrite letinE/= ge0_integral_mscale//= ger0_norm//; last first.
-    by rewrite invr_ge0// gauss_pdf_ge0.
+    by rewrite invr_ge0// normal_pdf_ge0.
   rewrite integral_dirac// diracT mul1e/= diracE epatch_indic/=.
   by rewrite indicE.
 rewrite integral_mgauss01//.
@@ -230,9 +152,24 @@ Qed.
 
 End gauss_lebesgue.
 
-(* assuming x > 0 *)
-Definition Gamma {R : realType} (x : R) : \bar R :=
-  \int[lebesgue_measure]_(t in `[0%R, +oo[%classic) (expR (- t) * powR t (x - 1))%:E.
+Notation left_continuous f :=
+  (forall x, f%function @ at_left x --> f%function x).
+
+Lemma left_continuousW (R : numFieldType) (f : R -> R) :
+  continuous f -> left_continuous f.
+Proof. by move=> cf x; exact/cvg_at_left_filter/cf. Qed.
+
+Section Gamma.
+Context {R : realType}.
+
+Let mu := @lebesgue_measure R.
+
+Definition Gamma (a : R) : \bar R :=
+  (\int[mu]_(x in `[0%R, +oo[) (powR x (a - 1) * expR (- x))%:E)%E.
+
+Let I n := \int[mu]_(x in `[0%R, +oo[) (x ^+ n * expR (- x))%:E.
+
+End Gamma.
 
 Definition Rfact {R : realType} (x : R) := Gamma (x + 1)%R.
 
