af628fe078668e6dbb1825885f9146049d8d0f2b
affeldt-aist <33154536+affeldt-aist@users.noreply.github.com>
2025-03-06 14:32:06 +0900

    Fixes 1505 (#1509)
    * typo
    
    * fixes #1505
    
    * fix nix badge
    
    Co-authored-by: Pierre Roux <pierre.roux@onera.fr>
    
    ---------
    
    Co-authored-by: Pierre Roux <pierre.roux@onera.fr>

--- theories/lebesgue_integral.v
+++ theories/lebesgue_integral.v
@@ -4922,7 +4922,7 @@ Implicit Types A : set (T1 * T2).
 
 Section xsection.
 Variables (pt2 : T2) (m2 : T1 -> {measure set T2 -> \bar R}).
-(* the generalization from m2 : {measure set T2 -> \bar R}t to
+(* the generalization from m2 : {measure set T2 -> \bar R} to
    T1 -> {measure set T2 -> \bar R} is needed to develop the theory
    of kernels; the original type was sufficient for the development
    of the theory of integration  *)
