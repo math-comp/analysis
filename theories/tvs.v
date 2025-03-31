18a2fe7129533bc02082815276e09384eb3d098a
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2025-03-31 17:08:37 +0900

    split normed_type.v

--- theories/tvs.v
+++ theories/tvs.v
@@ -5,8 +5,7 @@ From mathcomp Require Import rat interval zmodp vector fieldext falgebra.
 From mathcomp Require Import archimedean.
 From mathcomp Require Import boolp classical_sets functions cardinality.
 From mathcomp Require Import set_interval ereal reals interval_inference.
-From mathcomp Require Import topology prodnormedzmodule function_spaces.
-From mathcomp Require Import separation_axioms.
+From mathcomp Require Import topology function_spaces separation_axioms.
 
 (**md**************************************************************************)
 (* # Topological vector spaces                                                *)
