6c0899991827ba4c9aef8317cb7e78622678472e
Alessandro Bruni <brun@itu.dk>
2025-04-28 15:25:08 +0900

    removing independence.v

--- theories/sampling.v
+++ theories/sampling.v
@@ -11,7 +11,7 @@ From mathcomp Require Import reals ereal interval_inference topology normedtype.
 From mathcomp Require Import sequences realfun convex real_interval.
 From mathcomp Require Import derive esum measure exp numfun lebesgue_measure.
 From mathcomp Require Import lebesgue_integral kernel probability.
-From mathcomp Require Import hoelder independence.
+From mathcomp Require Import hoelder unstable.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -581,7 +581,7 @@ Proof.
 rewrite [X in measurable_fun _ X](_ : _
     = (fun x => \prod_(i < n) Tnth s (f i) x)); last first.
   by apply/funext => x; rewrite fct_prodE.
-by apply: measurable_prod => /= i _; apply/measurableT_comp => //.
+by apply: measurable_prod => /= i _; apply/measurableT_comp.
 Qed.
 
 HB.instance Definition _ m n (s : m.-tuple {mfun T >-> R}) (f : 'I_n -> 'I_m) :=
