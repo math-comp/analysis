a6ba1af8da4308a45e98d58738e898d1b3db3084
YOSHIHIRO Imai <y.imai@aist.go.jp>
2025-07-10 10:09:00 +0900

    chore: rocqnavi: :lipstick: :memo: :wrench: Fix a problem of links of the dependency graph on index.html by Rocqnavi (#1684)

--- etc/builddoc_dependency_dot.pl
+++ etc/builddoc_dependency_dot.pl
@@ -11,30 +11,31 @@ print "  subgraph cluster_lebesgue_integral { label=\"Lebesgue_integral\" }\n";
 while (<>) {
   if (m/([^\s]*)\.vo.*:(.*)/) {
     $dests = $2 ;
-    ($path,$src) = ($1 =~ s/\//\//rg =~ m/^(?:(.*\/))?([^.]*)$/);
-    if ($path =~ m/classical\//) {
-        $url="mathcomp.classical.$src.html";
-        print "subgraph cluster_classical { \"$path$src\"[label=\"$src\",URL=\"$url\",fillcolor=1]}\n";
-    }elsif ($path =~ m/reals\// or $path =~ m/reals_stdlib\//) {
-        print "subgraph cluster_reals { \"$path$src\"[label=\"$src\",fillcolor=2,fontcolor=white]}"
-    }elsif ($path =~ m/experimental_reals\//) {
-        print "subgraph cluster_experimental_reals { \"$path$src\"[label=\"$src\",fillcolor=3]}"
-    }elsif ($path =~ m/theories\/topology_theory\//) {
-        print "subgraph cluster_topology { \"$path$src\"[label=\"$src\",fillcolor=4,fontcolor=white]}"
-    }elsif ($path =~ m/theories\/normedtype_theory\//) {
-        print "subgraph cluster_normedtype { \"$path$src\"[label=\"$src\",fillcolor=9]}"
-    }elsif ($path =~ m/theories\/lebesgue_integral_theory\//) {
-        print "subgraph cluster_lebesgue_integral { \"$path$src\"[label=\"$src\",fillcolor=10,fontcolor=white]}"
-    }elsif ($path =~ m/theories\//) {
-       print "subgraph cluster_analysis { \"$path$src\"[label=\"$src\",fillcolor=5]}"
-    }elsif ($path =~ m/analysis_stdlib\/.*/) {
-       print "subgraph cluster_analysis { \"$path$src\"[label=\"$src\",fillcolor=5]}"
+    ($path,$src) = ($1 =~ s/\//\//rg =~ m/^(?:(.*)\/)?([^.]*)$/);
+    $url="mathcomp.$path.$src.html";
+    if ($path =~ m/classical/) {
+        print "subgraph cluster_classical { \"$path\/$src\"[label=\"$src\",URL=\"$url\",fillcolor=1]}\n";
+    }elsif ($path =~ m/reals/ or $path =~ m/reals_stdlib/) {
+        print "subgraph cluster_reals { \"$path\/$src\"[label=\"$src\",URL=\"$url\",fillcolor=2,fontcolor=white]}";
+    }elsif ($path =~ m/theories\/topology_theory/) {
+        $url="mathcomp.analysis.topology_theory.$src.html";
+        print "subgraph cluster_topology { \"$path\/$src\"[label=\"$src\",URL=\"$url\",fillcolor=4,fontcolor=white]}";
+    }elsif ($path =~ m/theories\/normedtype_theory/) {
+        $url="mathcomp.analysis.normedtype_theory.$src.html";
+        print "subgraph cluster_normedtype { \"$path\/$src\"[label=\"$src\",URL=\"$url\",fillcolor=9]}";
+    }elsif ($path =~ m/theories\/lebesgue_integral_theory/) {
+        $url="mathcomp.analysis.lebesgue_integral_theory.$src.html";
+        print "subgraph cluster_lebesgue_integral { \"$path\/$src\"[label=\"$src\",URL=\"$url\",fillcolor=10,fontcolor=white]}";
+    }elsif ($path =~ m/theories/) {
+       $url="mathcomp.analysis.$src.html";
+       print "subgraph cluster_analysis { \"$path\/$src\"[label=\"$src\",URL=\"$url\",fillcolor=5]}";
+    }elsif ($path =~ m/analysis_stdlib/) {
+       print "subgraph cluster_analysis { \"$path\/$src\"[label=\"$src\",URL=\"$url\",fillcolor=5]}";
     }else {
-        $url="/mathcomp.$src.html";
-        print "\"$path$src\"[label=\"$src\",URL=\"$url\",fillcolor=6,fontcolor=white]"
+        print "\"$path\/$src\"[label=\"$path=$src\",fillcolor=6,fontcolor=white]";
     }
     for my $dest (split(" ", $dests)) {
-        print "  \"$1\" -> \"$path$src\";\n" if ($dest =~ m/(.*)\.vo/);
+        print "  \"$1\" -> \"$path\/$src\";\n" if ($dest =~ m/(.*)\.vo/);
     }
   }
 }
