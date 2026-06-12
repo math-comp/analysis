08abd5324f3d366e5e8ea73cd84a580f03a904bc
Cyril Cohen <cyril.cohen@inria.fr>
2026-06-12 18:38:01 +0900

    WIP

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+         url = "https://github.com/CohenCyril/nixpkgs/archive/d84a118938cd8dccffe8a72083f98209d28d1af2.tar.gz";
+         sha256 = "0yn68b6lc6sxy3v0lgask5bfpwa1jm5xzxqlp12h0mlly9475hpk";
+       }
