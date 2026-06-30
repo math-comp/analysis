b6d8fd4563767ed7bf992e3fbde16ee66fcdc2af
adjevahi <arthur.djevahirdjian@ens-lyon.fr>
2026-06-30 10:46:17 +0200

    added preimageD

--- classical/functions.v
+++ classical/functions.v
@@ -2766,6 +2766,15 @@ Definition fctE :=
   (cstE, compE, opprfctE, addrfctE, mulrfctE, scalerfctE, exprfctE,
    zerofctE, onefctE).
 
+Lemma preimageD {T : Type} {Z : zmodType} (f g : T -> Z) (z : Z) : 
+(f \+ g) @^-1`[set z] 
+  = \bigcup_(a in range f) (f@^-1`[set a] `&` g@^-1`[set z-a]).
+Proof.
+rewrite/preimage eqEsubset; split=>[x /= fgz| x [a _ [/= <- -> ]]]. 
+  exists (f x)=>//; split=>//=. by rewrite -fgz addrC addKr.
+by rewrite subrKC.
+Qed.
+
 End function_space_lemmas.
 
 #[deprecated(since="mathcomp-analysis 1.17.0", use=scalerfctE)]
