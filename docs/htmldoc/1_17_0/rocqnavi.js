02eb082ccfd01e9b8b0ca6153a38534233841842
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2026-08-01 00:28:30 +0900

    gen doc for 1.17.0

---
+++ docs/htmldoc/1_17_0/rocqnavi.js
@@ -0,0 +1,65 @@
+
+function renderMarkdowns()
+{
+    const md = markdownit({html:true})
+          .use(texmath, { engine: katex,
+                          delimiters: 'dollars'} )
+          .use(markdownitDeflist);
+    const elements = document.querySelectorAll('.markdown,.md');
+    for (let elem of elements) {
+	      elem.innerHTML = md.render(elem.textContent);
+    }
+}
+
+function showDarkmodeWidget()
+{
+    new Darkmode({
+	      time: '0.1s',
+	      label: '🌓',
+    }).showWidget();
+}
+
+function setUpSavingDetails() {
+    $('details').on('toggle', function(event) {
+	      var id = $(this).attr('id')
+	      var isOpen = $(this).attr('open')
+	      window.localStorage.setItem('details-'+id, isOpen)
+    })
+
+    function setDetailOpenStatus(item) {
+	      if (item.includes('details-')) {
+	          var id = item.split('details-')[1];
+	          var status = window.localStorage.getItem(item)
+	          if (status == 'open') {
+		            $("#"+CSS.escape(id)).attr('open',true)
+	          }
+	      }
+    }
+
+    $( document ).ready(function() {
+	      for (var i = 0; i < localStorage.length; i++) {
+	          setDetailOpenStatus(localStorage.key(i));
+	      }
+    });
+}
+
+function setUpGraphZoom() {
+    // Zoom for Graphviz SVG
+    document.querySelectorAll("div.graph svg").forEach(svg => {
+        svgPanZoom(svg, {
+            zoomEnabled: true,
+            mouseWheelZoomEnabled: true,
+            controlIconsEnabled: true,
+            fit: true,
+            center: true
+        });
+    });
+};
+
+function init()
+{
+    renderMarkdowns();
+    showDarkmodeWidget();
+    setUpSavingDetails();
+    setUpGraphZoom();
+}
